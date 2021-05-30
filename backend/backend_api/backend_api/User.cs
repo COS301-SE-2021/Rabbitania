310e86dc661d318b9dffe7d10a5ce3fac28feaae
jmshltt <u16003251@tuks.co.za>
2021-05-30 21:02:16 +0200

    Added database connection and scaffolded dbcontext. Connection working.

---
+++ backend/backend_api/backend_api/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace backend_api
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+    }
+}
---
+++ backend/backend_api/backend_api/defaultdbContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace backend_api
+{
+    public partial class defaultdbContext : DbContext
+    {
+        public defaultdbContext()
+        {
+        }
+
+        public defaultdbContext(DbContextOptions<defaultdbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Host=db-rabbitania-do-user-9312303-0.b.db.ondigitalocean.com;Port=25060;Database=defaultdb;Username=doadmin;Password=ueg1ehmp1tgn650x;SslMode=Require;Trust Server Certificate=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "en_US.UTF-8");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasIndex(e => e.UserId, "users_user_id_uindex")
+                    .IsUnique();
+
+                entity.Property(e => e.UserId).HasColumnName("user_id");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnType("character varying")
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Firstname)
+                    .IsRequired()
+                    .HasColumnType("character varying")
+                    .HasColumnName("firstname");
+
+                entity.Property(e => e.Lastname)
+                    .IsRequired()
+                    .HasColumnType("character varying")
+                    .HasColumnName("lastname");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnType("character varying")
+                    .HasColumnName("password");
+
+                entity.Property(e => e.UserType)
+                    .IsRequired()
+                    .HasColumnType("character varying")
+                    .HasColumnName("user_type");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
