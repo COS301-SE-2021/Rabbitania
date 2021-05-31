6024fcc9895817fa36250af1cc4e229dbd063fa6
jmshltt <u16003251@tuks.co.za>
2021-05-31 22:13:18 +0200

    Added User Migrations

---
+++ backend/backend_api/backend_api/Migrations/20210531194223_added User entity.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend_api.Migrations
+{
+    public partial class addedUserentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    firstName = table.Column<string>(type: "text", nullable: true),
+                    lastName = table.Column<string>(type: "text", nullable: true),
+                    password = table.Column<string>(type: "text", nullable: true),
+                    isAdmin = table.Column<bool>(type: "boolean", nullable: false),
+                    userType = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
