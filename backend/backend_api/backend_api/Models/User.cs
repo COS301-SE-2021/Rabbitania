71aa769d285aea7954db094dc8a0a0fc61876282
jmshltt <u16003251@tuks.co.za>
2021-05-31 20:03:55 +0200

    Added User.cs in Models and RepositoryContext.cs for database management

---
+++ backend/backend_api/backend_api/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_api.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        
+        public string email { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string password { get; set; }
+        public bool isAdmin { get; set; }
+        public string userType { get; set; }
+    }
+}
---
+++ backend/backend_api/backend_api/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using backend_api.Models;
+
+namespace backend_api.Models
+{
+    public class RepositoryContext: DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+                    :base(options){}
+        
+        public DbSet<User> users { get; set; }
+        //other table dBsets go here
+
+    }
+}
