f9c67a055f78721a466ce868098c78c7ec054ca1
matth <u17005486@tuks.co.za>
2021-06-01 22:48:38 +0200

    Added Mock Seed data for User

--- backend/backend_api/backend_api/Migrations/20210601203453_UserMigration.cs
+++ backend/backend_api/backend_api/Migrations/20210601203453_UserMigration.cs
@@ -31,6 +31,20 @@ namespace backend_api.Migrations
                 {
                     table.PrimaryKey("PK_users", x => x.UserID);
                 });
+
+            migrationBuilder.InsertData(
+                table: "users",
+                columns: new[] { "UserID", "email", "employeeLevel", "firstname", "isAdmin", "isOnline", "lastname", "officeLocation", "phoneNumber", "pinnedUserIDs", "userDescription", "userImage", "userRoles" },
+                values: new object[,]
+                {
+                    { 1, "mattharty@retrorabbit.co.za", 4, "Matthew", true, true, "Harty", 0, "0834855754", null, "Hello there", "image.png", 0 },
+                    { 2, "rorymcilroy@retrorabbit.co.za", 5, "Rory", false, false, "McIlroy", 0, "0113445667", null, "Love golf.", "image1.png", 2 },
+                    { 3, "cgovender@retrorabbit.co.za", 6, "Castello", true, false, "Govender", 0, "0861830383", null, "Hey there!", "image2.png", 1 },
+                    { 4, "jameshulett@retrorabbit.co.za", 5, "James", false, false, "Hulett", 0, "0794756432", null, "Love gaming", "image3.png", 3 },
+                    { 5, "deannortje@retrorabbit.co.za", 3, "Dean", true, true, "Nortje", 0, "08374646378", null, "Keep moving forward - Standard Bank", "image4.png", 5 },
+                    { 6, "josephharraway@retrorabbit.co.za", 2, "Joe", true, false, "Harraway", 0, "0112345667", null, "Love coding", "image5.png", 2 },
+                    { 7, "devilliersmeiring@retrorabbit.co.za", 5, "DeVilliers", false, false, "Meiring", 0, "08611112567", null, "Fighting life", "image1.png", 0 }
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
