e41ad103bccae7219e1e31e824e527e5a0ddf810
matth <u17005486@tuks.co.za>
2021-06-02 19:28:40 +0200

    Added UserController UserEmails

---
+++ backend/backend_api/backend_api/Migrations/20210602163025_newMigration.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend_api.Migrations
+{
+    public partial class newMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserEmails",
+                columns: table => new
+                {
+                    userEmailsID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    userEmail = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserEmails", x => x.userEmailsID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    firstname = table.Column<string>(type: "text", nullable: true),
+                    lastname = table.Column<string>(type: "text", nullable: true),
+                    phoneNumber = table.Column<string>(type: "text", nullable: true),
+                    pinnedUserIDs = table.Column<List<int>>(type: "integer[]", nullable: true),
+                    userImage = table.Column<string>(type: "text", nullable: true),
+                    userDescription = table.Column<string>(type: "text", nullable: true),
+                    isOnline = table.Column<bool>(type: "boolean", nullable: false),
+                    isAdmin = table.Column<bool>(type: "boolean", nullable: false),
+                    employeeLevel = table.Column<int>(type: "integer", nullable: false),
+                    userRoles = table.Column<int>(type: "integer", nullable: false),
+                    officeLocation = table.Column<int>(type: "integer", nullable: false),
+                    userEmailsID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.UserID);
+                    table.ForeignKey(
+                        name: "FK_users_UserEmails_userEmailsID",
+                        column: x => x.userEmailsID,
+                        principalTable: "UserEmails",
+                        principalColumn: "userEmailsID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_userEmailsID",
+                table: "users",
+                column: "userEmailsID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "UserEmails");
+        }
+    }
+}
