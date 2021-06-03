56aab012b8d008a64e05800b25c1b174f3c83737
matth <u17005486@tuks.co.za>
2021-06-03 21:27:34 +0200

    UserController

--- backend/backend_api/backend_api/Controllers/UserController.cs
+++ backend/backend_api/backend_api/Controllers/UserController.cs
@@ -20,25 +20,18 @@ namespace backend_api.Controllers
             _context = context;
         }
 
-        /// <summary>
-        ///     Get all users, for Admin purposes.
-        /// </summary>
-        /// <returns> List of all users </returns>
-        [HttpGet("/api/GetUsers")]
+        // GET: api/User
+        [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
             return await _context.users.ToListAsync();
         }
 
-        /// <summary>
-        ///     Gets the user profile by their associated userIDs
-        /// </summary>
-        /// <param name="userID"></param>
-        /// <returns> A User </returns>
-        [HttpGet("/api/ViewProfile/{userID}")]
-        public async Task<ActionResult<User>> GetUser(int userID)
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
         {
-            var user = await _context.users.FindAsync(userID);
+            var user = await _context.users.FindAsync(id);
 
             if (user == null)
             {
@@ -48,17 +41,12 @@ namespace backend_api.Controllers
             return user;
         }
 
-        /// <summary>
-        ///    Retrieves a user profile and sets the updates to the DB
-        ///    - Front-End will deal with the editing profile UI and information passed back.
-        /// </summary>
-        /// <param name="userID"></param>
-        /// <param name="user"></param>
-        /// <returns></returns>
-        [HttpPut("/api/EditProfile/{userID}")]
-        public async Task<IActionResult> PutUser(int userID, User user)
+        // PUT: api/User/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
         {
-            if (userID != user.UserID)
+            if (id != user.UserID)
             {
                 return BadRequest();
             }
@@ -71,10 +59,12 @@ namespace backend_api.Controllers
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!UserExists(userID)){
+                if (!UserExists(id))
+                {
                     return NotFound();
                 }
-                else{
+                else
+                {
                     throw;
                 }
             }
@@ -82,41 +72,33 @@ namespace backend_api.Controllers
             return NoContent();
         }
 
-        /// <summary>
-        ///     Creates a user and insert said into the database.
-        /// </summary>
-        /// <param name="user"></param>
-        /// <returns>The created User </returns>
-        [HttpPost("/api/CreateUser")]
-        public async Task<ActionResult<User>> CreateUser(User user)
+        // POST: api/User
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
         {
             _context.users.Add(user);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetUser", new { id = user.UserID }, user);
         }
-        
-        /// <summary>
-        ///     Logs user into the system using Google OAuth
-        ///     - Changes isOnline status to true
-        /// </summary>
-        [HttpGet("/api/Login")]
-        public void LoginUser()
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
         {
-            //TODO: Implement Login functionality using Google OAuth
+            var user = await _context.users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
-        
-        /// <summary>
-        ///     Logs a user out of the system
-        ///     - Changes the isOnline status to false
-        /// </summary>
-        [HttpGet("/api/Logout")]
-        public void LogoutUser()
-        {
-            //TODO: Implement Logout functionality using Google OAuth
-        }
-        
-        
+
         private bool UserExists(int id)
         {
             return _context.users.Any(e => e.UserID == id);
