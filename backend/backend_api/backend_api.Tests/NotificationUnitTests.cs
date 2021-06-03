0546f2d4db7cb8bd7b6888b0abc9fbb9bf565e66
Deannortje <u17326622@tuks.co.za>
2021-06-03 09:23:49 +0200

    NotificationUnitTests.cs test changes

--- backend/backend_api/backend_api.Tests/NotificationUnitTests.cs
+++ backend/backend_api/backend_api.Tests/NotificationUnitTests.cs
@@ -1,7 +1,39 @@
-﻿namespace backend_api.Tests
+﻿using System;
+using backend_api.Models;
+using backend_api.Models.Notifications;
+using Xunit;
+namespace backend_api.Tests
 {
     public class NotificationUnitTests
     {
+        private readonly Notification _sutNotification;
+
+        public NotificationUnitTests()
+        {
+            
+        }
+        
+        [Fact]
+        public void GetNotificationId()
+        {
+    
+        }
+        [Fact]
+        public void NotificationContent()
+        {
+            
+        }
+        [Fact]
+        public void NotificationType()
+        {
+            
+        }
+        [Fact]
+        public void DateCreated()
+        {
+        }
         
     }
+    
+   
 }
