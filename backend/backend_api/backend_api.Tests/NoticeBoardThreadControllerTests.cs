080ecd90e059215a5a3b66a31bbd4d37ea511b0b
DeMeiring <u17074292@tuks.co.za>
2021-06-03 09:57:34 +0200

    Mock test for NoticeBoardThreadControllerGetAll endpoint

---
+++ backend/backend_api/backend_api.Tests/NoticeBoardThreadControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using Xunit;
+
+namespace backend_api.Tests
+{
+    public class NoticeBoardThreadControllerTests
+    {
+        [Fact]
+        public async void getAllMethodTest()
+        {
+            var _client = new TestClientProvider()._client;
+
+            var response = await _client.GetAsync("api/NoticeBoardThread");
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+    }
+}
