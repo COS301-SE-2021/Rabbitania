434268be8ce5bbda1af8c082498f884fc3949ed9
jmshltt <u16003251@tuks.co.za>
2021-06-03 02:48:10 +0200

    Installed Moq and added some unit tests

--- backend/backend_api/backend_api.Tests/UserTests.cs
+++ backend/backend_api/backend_api.Tests/UserTests.cs
@@ -1,6 +1,7 @@
 ﻿using backend_api.Models;
 using Xunit;
 
+
 namespace backend_api.Tests
 {
     public class UserTests
@@ -12,23 +13,23 @@ namespace backend_api.Tests
             _sut = new User();
         }
 
-        // [Fact]
-        // public void VeryFirstTest()
-        // {
-        //     
-        // }
-        // [Fact]
-        // public void createUser()
-        // {
-        //     User testUser = new User();
-        //     Assert.NotNull(testUser);
-        // }
-        //
-        // [Fact]
-        // public void setUserID()
-        // {
-        //     this._sut.UserID = 1;
-        //     Assert.Equal(1, _sut.UserID);
-        // }
+        [Fact]
+        public void VeryFirstTest()
+        {
+            
+        }
+        [Fact]
+        public void createUser()
+        {
+            User testUser = new User();
+            Assert.NotNull(testUser);
+        }
+        
+        [Fact]
+        public void setUserID()
+        {
+            this._sut.UserID = 1;
+            Assert.Equal(1, _sut.UserID);
+        }
     }
 }
