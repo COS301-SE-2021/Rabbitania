3c8594a063a86a73ae9e6883a85b120d2e115671
Deannortje <u17326622@tuks.co.za>
2021-06-03 09:45:23 +0200

    NotificationUnitTests.cs Added tests for setters

--- backend/backend_api/backend_api.Tests/NotificationUnitTests.cs
+++ backend/backend_api/backend_api.Tests/NotificationUnitTests.cs
@@ -7,11 +7,52 @@ namespace backend_api.Tests
     public class NotificationUnitTests
     {
         private readonly Notification _sutNotification;
+        private readonly NotificationTypeEnum _sutNotificationTypeEnum;
+        private DateTime _dateTime;
 
         public NotificationUnitTests()
         {
-            
+            this._sutNotification = new Notification();
+            this._sutNotificationTypeEnum = new NotificationTypeEnum();
+            this._dateTime = new DateTime();
+
         }
+
+        [Fact]
+        public void CreateNewNotification()
+        {
+            
+            SetNotificationId();
+            SetNotificationContent();
+            SetNotificationType();
+            SetNotificationDateCreated();
+        }
+        [Fact]
+        public void SetNotificationId()
+        {
+            this._sutNotification.notificationID = 100;
+        }
+        
+        [Fact]
+        public void SetNotificationContent()
+        {
+            this._sutNotification.notificationContent = "Notification unitTest for Setting content ";
+        }
+        
+        [Fact]
+        public void SetNotificationType()
+        {
+            this._sutNotification.notificationType = _sutNotificationTypeEnum;
+        }
+        
+        [Fact]
+        public void SetNotificationDateCreated()
+        {
+            this._sutNotification.dateCreated = _dateTime;
+        }
+        
+        
+        
         
         [Fact]
         public void GetNotificationId()
