82c1751717f3667042c071886187f0a4beb9a0ec
MattHarty <matthewnharty@gmail.com>
2021-06-09 00:02:06 +0200

    Pushed controller and migrations, somehow it was added to unprovisioned files

---
+++ backend/backend_api/Notifications/Controller/NotificationsController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using backend_api.Models.Notifications;
+using backend_api.Notifications.Data;
+
+namespace backend_api.Notifications.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly NotificationContext _context;
+
+        public NotificationsController(NotificationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Notifications
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Notification>>> GetNotifications()
+        {
+            return await _context.Notifications.ToListAsync();
+        }
+
+        // GET: api/Notifications/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Notification>> GetNotification(int id)
+        {
+            var notification = await _context.Notifications.FindAsync(id);
+
+            if (notification == null)
+            {
+                return NotFound();
+            }
+
+            return notification;
+        }
+
+        // PUT: api/Notifications/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNotification(int id, Notification notification)
+        {
+            if (id != notification.NotificationId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(notification).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NotificationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Notifications
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Notification>> PostNotification(Notification notification)
+        {
+            _context.Notifications.Add(notification);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNotification", new { id = notification.NotificationId }, notification);
+        }
+
+        // DELETE: api/Notifications/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteNotification(int id)
+        {
+            var notification = await _context.Notifications.FindAsync(id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+
+            _context.Notifications.Remove(notification);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool NotificationExists(int id)
+        {
+            return _context.Notifications.Any(e => e.NotificationId == id);
+        }
+    }
+}
