83d3304a5c98f992c4edc74cb34d58ad36433fc5
James <u16003251@tuks.co.za>
2021-06-09 00:52:19 +0200

    Implemented User Service

---
+++ backend/backend_api/User/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using backend_api.User.Data.User;
+
+namespace backend_api.User.Services
+{
+    public class UserService: IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepo)
+        {
+            _userRepository = userRepo;
+        }
+        //Logical functions
+        public void verifyLogin()
+        {
+            
+        }
+    }
+}
