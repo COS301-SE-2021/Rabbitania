db0c31eb1e5360850a174f728a9b9020c65a3638
James <u16003251@tuks.co.za>
2021-06-11 22:54:27 +0200

    Added TODOs to complete

--- backend/backend_api/User/Models/Requests/GetUserRequest.cs
+++ backend/backend_api/User/Models/Requests/GetUserRequest.cs
@@ -1,21 +1,38 @@
 ﻿using System;
 using System.Data;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using backend_api.User.Models.Responses;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace backend_api.User.Models.Requests
 {
     public class GetUserRequest
     {
-        private String jwt;
+        //TODO: implement request to use jwt token from google Oauth
+        //private JsonWebToken jwt;
+        
+        // public GetUserRequest(JsonWebToken jwt)
+        // {
+        //     this.jwt = jwt;
+        // }
 
-        public GetUserRequest(String jwt)
+        // public JsonWebToken getToken()
+        // {
+        //     return jwt;
+        // }
+        //----------------------------------------------------------------------------------
+        private String userEmail;
+
+        public GetUserRequest(String email)
         {
-            this.jwt = jwt;
+            this.userEmail = email;
         }
 
-        public String getToken()
+        public String getEmail()
         {
-            return jwt;
+            return this.userEmail;
         }
-
     }
 }
--- backend/backend_api/User/Services/UserService.cs
+++ backend/backend_api/User/Services/UserService.cs
@@ -1,4 +1,8 @@
-﻿using backend_api.User.Data;
+﻿using System;
+using backend_api.User.Data;
+using backend_api.User.Models.Requests;
+using backend_api.User.Models.Responses;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace backend_api.User.Services
 {
@@ -15,6 +19,18 @@ namespace backend_api.User.Services
         public void verifyLogin()
         {
             
+        }
+
+        public GetUserResponse getUser(GetUserRequest request)
+        {
+            //TODO: implement GetUSerResponse to use the jwt token from google login API
+            //JsonWebToken token = request.getToken();
+            //String email = token.getEmail();
+
+            String email = request.getEmail();
+            //search for user
+            Models.User.User user = _userRepository.GetUser();
+            GetUserResponse response = new GetUserResponse();
         }
     }
 }
