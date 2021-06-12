379044c0350b4d3c2c1b0c04dc5390493686ccec
James <u16003251@tuks.co.za>
2021-06-13 00:26:33 +0200

    Updated response

--- backend/backend_api/Models/Auth/Responses/GoogleResponse.cs
+++ backend/backend_api/Models/Auth/Responses/GoogleResponse.cs
@@ -1,7 +1,12 @@
-﻿namespace backend_api.Models.Auth.Responses
+﻿using System;
+
+namespace backend_api.Models.Auth.Responses
 {
     public class GoogleResponse
     {
+        private String email { get; set; }
+        private int token { get; set; }
+        private String name { get; set; }
         
     }
 }
