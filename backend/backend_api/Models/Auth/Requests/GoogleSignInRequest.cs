7d85e1656403c20ed58d99cc4c93110d45dad542
James <u16003251@tuks.co.za>
2021-06-14 20:40:32 +0200

    Google Authentication user exists checks

--- backend/backend_api/Models/Auth/Requests/GoogleSignInRequest.cs
+++ backend/backend_api/Models/Auth/Requests/GoogleSignInRequest.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.IdentityModel.JsonWebTokens;
+﻿using System;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace backend_api.Models.Auth.Requests
 {
     public class GoogleSignInRequest
     {
         private JsonWebToken jwt { get; set; }
+        private String _name { get; set; }
+        private String _email { get; set; }
     }
 }
---
+++ backend/backend_api/Services/Auth/AuthService.cs
@@ -0,0 +1,21 @@
+﻿using backend_api.Data.User;
+using backend_api.Models.Auth.Requests;
+using backend_api.Models.Auth.Responses;
+
+namespace backend_api.Services.Auth
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepository _repository;
+
+        public GoogleResponse GoogleAuthResponse(GoogleSignInRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AuthService(IUserRepository repository)
+        {
+            this._repository = repository;
+        }
+    }
+}
