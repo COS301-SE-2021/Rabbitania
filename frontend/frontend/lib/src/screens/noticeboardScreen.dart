9bbb9a2644b585c634b5ee9cf890f4268c2d6397
Deannortje <u17326622@gmail.com>
2021-06-15 22:50:57 +0200

    Changes made for noticeboard

--- frontend/frontend/lib/src/screens/noticeboardScreen.dart
+++ frontend/frontend/lib/src/screens/noticeboardScreen.dart
@@ -7,13 +7,13 @@ class NoticeBoard extends StatefulWidget {
   }
 }
 
-
 class _NoticeBoard extends State<NoticeBoard> {
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        child: noticeboardCard(),
+        height: 300,
+        child: NoticeboardCard(),
       ),
     );
   }
--- frontend/frontend/lib/src/widgets/card_item.dart
+++ frontend/frontend/lib/src/widgets/card_item.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
-class noticeboardCard extends StatelessWidget {
+class NoticeboardCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Card(
-        child: Text("Hello Rabbitania")
+        color: Colors.white,
+        shadowColor: Colors.green,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0)
+        ),
+        clipBehavior: Clip.antiAlias,
+        elevation: 10,
+          child: Column(
+            children: [
+              ListTile(
+                contentPadding: EdgeInsets.all(10.0),
+                leading: Icon(Icons.account_circle_outlined, size: 70, color: Colors.green,),
+                title: Text( "API TEXT",
+                  style: TextStyle(letterSpacing: 3.0),
+                ),
+                subtitle: Text("INSERT API TEXT"),
+              ),
 
+            ],
+          ),
       ),
     );
   }
