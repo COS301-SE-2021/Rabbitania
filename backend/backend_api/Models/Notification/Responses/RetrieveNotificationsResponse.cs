89868a42a4f3f3a6379fa5c0d9243b1b967400ea
Matt Harty <matthewnharty@gmail.com>
2021-06-16 16:05:51 +0200

    Fixed RetrieveNotification

--- backend/backend_api/Models/Notification/Responses/RetrieveNotificationsResponse.cs
+++ backend/backend_api/Models/Notification/Responses/RetrieveNotificationsResponse.cs
@@ -4,19 +4,11 @@ namespace backend_api.Models.Notification.Responses
 {
     public class RetrieveNotificationsResponse
     {
-        private string _message;
         private IEnumerable<Notification> _notifications;
         
-        public RetrieveNotificationsResponse(string message, IEnumerable<Notification> notifications)
+        public RetrieveNotificationsResponse(IEnumerable<Notification> notifications)
         {
-            this._message = message;
             this._notifications = notifications;
-        }
-
-        public string Message
-        {
-            get => _message;
-            set => _message = value;
         }
 
         public IEnumerable<Notification> Notifications
