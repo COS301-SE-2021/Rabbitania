e216ebf7f7e4e990f719ff5fb6c298eef5e8d0be
James <u16003251@tuks.co.za>
2021-06-16 23:10:00 +0200

    Started backend auth unit tests

---
+++ backend/backend_api.Tests/AuthUnitTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using backend_api.Data.User;
+using backend_api.Exceptions.Auth;
+using backend_api.Models;
+using backend_api.Models.Auth.Requests;
+using backend_api.Models.Auth.Responses;
+using backend_api.Models.User;
+using backend_api.Services.Auth;
+using Moq; 
+using Xunit;
+using Xunit.Sdk;
+
+namespace backend_api.Tests
+{
+    public class AuthUniTests
+    {
+        private AuthService _authService;
+        private readonly Mock<IUserRepository> _userRepositoryMock = new Mock<IUserRepository>();
+
+        [Fact]
+        public void invalidDomainLogin()
+        {
+            //Arrange
+            String email = "test@gmail.com";
+            GoogleSignInRequest request1 = new GoogleSignInRequest(email);
+            
+            //Act
+            var response = _authService.CheckEmailDomain(request1);
+            //Assert
+            Assert.NotNull(response);
+            Assert.IsType<DomainResponse>(response);
+        }
+        //TODO: finish test
+        [Fact]
+        public void invalidEmailLogin()
+        {
+            String email = "hi@castellodev.co.za";
+            
+        }
+        //TODO: Check why I get a TestClassException error
+        public AuthUniTests(AuthService authService)
+        {
+            _authService = new AuthService(_userRepositoryMock.Object);
+        }
+    }
+    
+}
