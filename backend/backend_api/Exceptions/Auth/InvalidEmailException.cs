264b658a191fc6b7c9bfa1567697aa0ef6312279
Matt Harty <matthewnharty@gmail.com>
2021-06-16 12:40:46 +0200

    Added custom exceptions

---
+++ backend/backend_api/Exceptions/Auth/InvalidEmailException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace backend_api.Exceptions.Auth
+{
+    public class InvalidEmailException : Exception
+    {
+        public InvalidEmailException()
+        {
+            
+        }
+
+        public InvalidEmailException(string message) : base(message)
+        {
+            
+        }
+        
+        public InvalidEmailException(string message, Exception inner) : base(message, inner)
+        {
+            
+        }
+    }
+}
