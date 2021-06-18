0f1fb588e13c6b8d8944426fe6de09f075fa4a19
DeVilliers <u17074292@tuks.co.za>
2021-06-18 20:49:21 +0200

    additional info scrren basic layout

--- frontend/frontend/lib/src/widgets/profile_picture_widget.dart
+++ frontend/frontend/lib/src/widgets/profile_picture_widget.dart
@@ -3,9 +3,23 @@ import 'package:flutter/material.dart';
 
 class ProfilePicture extends StatelessWidget {
   final user = FirebaseAuth.instance.currentUser!;
-  Widget build(context) => CircleAvatar(
-        backgroundColor: Colors.transparent,
-        radius: 30,
-        backgroundImage: NetworkImage(user.photoURL!),
+  Widget build(context) => Container(
+        margin: const EdgeInsets.all(15.0),
+        padding: const EdgeInsets.all(3.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.transparent,
+              radius: 30,
+              backgroundImage: NetworkImage(user.photoURL!),
+            ),
+            VerticalDivider(),
+            Text(user.email!,
+                style: TextStyle(
+                  color: Color.fromRGBO(171, 255, 79, 1),
+                )),
+          ],
+        ),
       );
 }
