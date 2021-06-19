4d682e3c78dac17ed108b19180b933a377d2c2d2
James <u16003251@tuks.co.za>
2021-06-19 19:39:47 +0200

    Created in memory db for users

--- backend/backend_api.Tests/Auth/IntegrationTests/AuthServiceIntegrationTests.cs
+++ backend/backend_api.Tests/Auth/IntegrationTests/AuthServiceIntegrationTests.cs
@@ -1,14 +1,35 @@
 ﻿using System.Net.Http;
+using backend_api.Data.Notification;
+using backend_api.Data.User;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace backend_api.Tests.Auth.IntegrationTests
 {
     public class AuthServiceIntegrationTests
     {
-        private HttpClient client;
+        private HttpClient _client;
 
         public AuthServiceIntegrationTests()
         {
-            
+            // App factory
+            // Using memory virtual Database
+            var authFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(UserContext));
+                        services.AddDbContext<UserContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("RabbitaniaDB");
+                        });
+                    });
+                });
+            _client = authFactory.CreateClient();
         }
+        
     }
 }
