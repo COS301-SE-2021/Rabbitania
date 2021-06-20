4972ce23ee7d1066d4817949f52351e74e4423ae
Matt Harty <matthewnharty@gmail.com>
2021-06-20 11:30:56 +0200

    Got 2 tests working

--- backend/backend_api.Tests/NotificationTests/IntegrationTests/NotificationControllerTests.cs
+++ backend/backend_api.Tests/NotificationTests/IntegrationTests/NotificationControllerTests.cs
@@ -27,18 +27,31 @@ namespace backend_api.Tests.NotificationTests.IntegrationTests
         }
 
 
-        [Fact(DisplayName = "Retrieve Notifications should return OK when user exists")]
-        public async Task NotificationController_RetrieveNotificationsEndpoint_ShouldNotReturnNotificationsOfTheUserIfUserExists()
+        [Fact(DisplayName = "Retrieve Notifications should be an internal server error when user doesn't exist")]
+        public async Task NotificationController_RetrieveNotificationsEndpoint_ShouldNotReturnNotificationsOfTheUserIfUserDoesNotExists()
         {
             // Arrange
-            var requestUrl = "api/Notifications/RetrieveNotifications?UserId=1";
+            var response = await _client.GetAsync("/api/Notifications/RetrieveNotifications?UserId=0");
 
             // Act
-            var responseMessage = await _client.GetAsync(requestUrl);
-            var responseMessageContent = responseMessage.Content;
-
+            var resString = await response.Content.ReadAsStringAsync();
+            
             //Assert
-            Assert.NotEqual("", responseMessage.ToString());
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+        }
+        
+        [Fact(DisplayName = "Retrieve Notifications should return OK when user exists")]
+        public async Task NotificationController_RetrieveNotificationsEndpoint_ShouldReturnNotificationsOfTheUserIfUserExists()
+        {
+            // Arrange
+            var response = await _client.GetAsync("/api/Notifications/RetrieveNotifications?UserId=1");
+
+            // Act
+            response.EnsureSuccessStatusCode();
+            var resString = await response.Content.ReadAsStringAsync();
+            
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
 
     }
