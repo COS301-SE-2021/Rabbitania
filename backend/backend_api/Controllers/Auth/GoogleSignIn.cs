c2227bf841ca62e56f8524660c41183e51787e51
Matt Harty <matthewnharty@gmail.com>
2021-06-28 10:38:19 +0200

    General clean up

--- backend/backend_api/Controllers/Auth/GoogleSignIn.cs
+++ backend/backend_api/Controllers/Auth/GoogleSignIn.cs
@@ -50,28 +50,7 @@ namespace backend_api.Controllers.Auth
         [Route("GoogleLogin")]
         public async Task<ActionResult> GoogleResponse(GoogleSignInRequest request)
         {
-            //Task<String>
-            // var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            // var claims = result.Principal.Identities.FirstOrDefault().Claims.Select(claim => new
-            // {
-            //     claim.Issuer,
-            //     claim.OriginalIssuer,
-            //     claim.Type,
-            //     claim.Value
-            // }).ToList();
-            //
-            // //Creating Google response object
-            // var token = claims[0].Value;
-            // var email = claims[(claims.Count) - 1].Value;
-            // var givenName = claims[1].Value;
-            // var name = claims[2].Value;
-            // var surname = "";
-            // //not every account has provided a surname
-            // if (claims.Capacity > 4)
-            // {
-            //     surname = claims[3].Value;
-            // }
-            
+
             var email = request.Email;
 
             GoogleResponse response = new GoogleResponse();
