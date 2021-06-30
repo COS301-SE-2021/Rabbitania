e23940ff5788b795558a2965f11bf774c39f390d
Matt Harty <matthewnharty@gmail.com>
2021-06-30 13:02:43 +0200

    Finished implementing send email

---
+++ backend/backend_api/Exceptions/Notifications/EmailFailedToSendException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace backend_api.Exceptions.Notifications
+{
+    public class EmailFailedToSendException : Exception
+    {
+        public EmailFailedToSendException()
+        {
+            
+        }
+
+        public EmailFailedToSendException(string message) : base(message)
+        {
+            
+        }
+        
+        public EmailFailedToSendException(string message, Exception inner) : base(message, inner)
+        {
+            
+        }
+    }
+}
