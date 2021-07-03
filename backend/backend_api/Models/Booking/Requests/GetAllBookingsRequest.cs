30fef9ff862f9c7ece47f7afc68561b30fbc748b
James <u16003251@tuks.co.za>
2021-07-03 22:04:20 +0200

    Implemented GetAllBookingsRequest.cs

---
+++ backend/backend_api/Models/Booking/Requests/GetAllBookingsRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace backend_api.Models.Booking.Requests
+{
+    public class GetAllBookingsRequest
+    {
+        private int userID;
+        private DateTime date;
+
+        public GetAllBookingsRequest(DateTime date)
+        {
+            this.date = date;
+        }
+
+        public GetAllBookingsRequest(int userId, DateTime date)
+        {
+            userID = userId;
+            this.date = date;
+        }
+
+        public DateTime Date
+        {
+            get => date;
+            set => date = value;
+        }
+
+        public GetAllBookingsRequest()
+        {
+        }
+
+        public GetAllBookingsRequest(int userId)
+        {
+            userID = userId;
+        }
+
+        public int UserId
+        {
+            get => userID;
+            set => userID = value;
+        }
+    }
+}
