92be1fc812f148be0b56b4192b1dd45fd458acd5
James <u16003251@tuks.co.za>
2021-07-03 18:12:56 +0200

    Completed Booking Model

---
+++ backend/backend_api/Models/Booking/Booking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using backend_api.Models.User;
+
+namespace backend_api.Models.Booking
+{
+    public class Booking 
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BookingID { get; set; }
+        
+        public DateTime BookingDate { get; set; }
+        public float Duration { get; set; }
+        
+        [ForeignKey("User")] 
+        public int UserID { get; set; }
+        
+        public OfficeLocation OfficeLocation { get; set; } 
+    }
+}
