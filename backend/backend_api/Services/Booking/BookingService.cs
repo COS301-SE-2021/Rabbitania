66bc18ea47b50a7c3340e4b7decd196fa07a43c6
jmshltt <u16003251@tuks.co.za>
2021-07-04 19:47:26 +0200

    Completed UpdateBooking service function

--- backend/backend_api/Services/Booking/BookingService.cs
+++ backend/backend_api/Services/Booking/BookingService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net;
+using System.Threading.Tasks;
 using backend_api.Data.Booking;
 using backend_api.Models.Booking.Requests;
 using backend_api.Models.Booking.Responses;
@@ -20,7 +21,15 @@ namespace backend_api.Services.Booking
         }
         public async Task<UpdateBookingResponse> UpdateBooking(UpdateBookingRequest request)
         {
-            
+            var resp = await _bookingRepository.UpdateBooking(request);
+            if (resp == HttpStatusCode.Accepted)
+            {
+                return new UpdateBookingResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return new UpdateBookingResponse(HttpStatusCode.NotFound);
+            }
         }
 
         public async Task<CancelBookingResponse> CancelBooking(CancelBookingRequest request)
--- backend/backend_api/Services/Booking/IBookingService.cs
+++ backend/backend_api/Services/Booking/IBookingService.cs
@@ -11,7 +11,7 @@ namespace backend_api.Services.Booking
         Task<CancelBookingResponse> CancelBooking(CancelBookingRequest request);
         
         Task<UpdateBookingResponse> UpdateBooking(UpdateBookingRequest request);
-        
+
         Task<GetBookingResponse> ViewBooking(GetBookingRequest request);
         
         Task<GetAllBookingsResponse> ViewAllBookings(GetAllBookingsRequest request);
