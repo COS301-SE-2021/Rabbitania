aad2aafb905a89850f81d908d6b2d30a4a9cbf58
jmshltt <u16003251@tuks.co.za>
2021-07-04 13:23:29 +0200

    Began implementation of booking service

---
+++ backend/backend_api/Services/Booking/BookingService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using backend_api.Models.Booking.Requests;
+
+namespace backend_api.Services.Booking
+{
+    public class BookingService: IBookingService
+    {
+
+        public Task<CreateBookingResponse> CreateBooking(CreateBookingRequest request)
+        {
+            
+        }
+
+    }
+}
---
+++ backend/backend_api/Services/Booking/IBookingService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using backend_api.Models.Booking.Requests;
+
+namespace backend_api.Services.Booking
+{
+    public interface IBookingService
+    {
+        Task<CreateBookingResponse> CreateBooking(CreateBookingRequest request);
+    }
+}
