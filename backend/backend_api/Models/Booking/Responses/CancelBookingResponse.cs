7e8fdb584aed9a849c809465f2309fde28b1b6d8
Matt Harty <matthewnharty@gmail.com>
2021-07-04 13:43:28 +0200

    Finished response objects

--- backend/backend_api/Models/Booking/Responses/CancelBookingResponse.cs
+++ backend/backend_api/Models/Booking/Responses/CancelBookingResponse.cs
@@ -1,7 +1,26 @@
-﻿namespace backend_api.Models.Booking.Responses
+﻿using System.Net;
+
+namespace backend_api.Models.Booking.Responses
 {
     public class CancelBookingResponse
     {
-        
+        /// <summary>
+        ///     Will return status code response to confirm if a booking
+        ///     was indeed cancelled or not
+        ///     + Status code return 200 (Ok) or
+        ///     + Status code return 400 (Bad request) 
+        /// </summary>
+        private HttpStatusCode _response;
+
+        public CancelBookingResponse(HttpStatusCode response)
+        {
+            _response = response;
+        }
+
+        public HttpStatusCode Response
+        {
+            get => _response;
+            set => _response = value;
+        }
     }
 }
--- backend/backend_api/Models/Booking/Responses/GetBookingResponse.cs
+++ backend/backend_api/Models/Booking/Responses/GetBookingResponse.cs
@@ -2,6 +2,20 @@
 {
     public class GetBookingResponse
     {
-        
+        /// <summary>
+        ///     Will return a response with a booking object
+        /// </summary>
+        private Booking _booking;
+
+        public GetBookingResponse(Booking booking)
+        {
+            _booking = booking;
+        }
+
+        public Booking Booking
+        {
+            get => _booking;
+            set => _booking = value;
+        }
     }
 }
