8950237340e7af66d42caaef1192e7c829f7c2ba
jmshltt <u16003251@tuks.co.za>
2021-07-04 12:03:24 +0200

    Updated GetBookings endpoint

--- backend/backend_api/Controllers/Booking/BookingController.cs
+++ backend/backend_api/Controllers/Booking/BookingController.cs
@@ -2,17 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend_api.Controllers.Booking
 {
-    [Route("api/[controller]")]
+    [AllowAnonymous, Route("api/[controller]")]
     [ApiController]
     public class BookingController : ControllerBase
     {
         // GET: api/Booking
         [HttpGet]
+        [Route("GetBookings")]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
