518eb05b16fca0dc7cc9ffd08e1c3c119d694e47
jmshltt <u16003251@tuks.co.za>
2021-07-04 21:49:34 +0200

    Completed Get endpoints for booking controller

--- backend/backend_api/Controllers/Booking/BookingController.cs
+++ backend/backend_api/Controllers/Booking/BookingController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend_api.Exceptions.Booking;
 using backend_api.Models.Booking.Requests;
 using backend_api.Models.Booking.Responses;
 using backend_api.Services.Booking;
@@ -26,16 +27,31 @@ namespace backend_api.Controllers.Booking
             _bookingService = bookingService;
         }
         
-        // GET: api/GetBookings
+        /// <summary>
+        ///     API endpoint for retrieving bookings belonging to a user
+        ///     Checks if the given request is valid and then queries the service
+        ///     If the user cannot be found an InvalidBookingException is thrown
+        ///     An Http response code is returned.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>Http response code and json object</returns>
         [HttpGet]
         [Route("GetBookings")]
         public async Task<ActionResult> GetAllBookings([FromQuery] GetAllBookingsRequest request)
         {
             if (request != null)
             {
-                var bookings = await _bookingService.ViewAllBookings(request);
-                var list = bookings.Bookings.ToList();
-                return Ok(list);
+                try
+                {
+                    var bookings = await _bookingService.ViewAllBookings(request);
+                    var list = bookings.Bookings.ToList();
+                    return Ok(list);
+                }
+                catch (InvalidBookingException e)
+                {
+                    throw e;
+                }
+                
             }
             else
             {
@@ -43,25 +59,37 @@ namespace backend_api.Controllers.Booking
             }
             
         }
-
-       
-
-        // POST: api/Booking
-        [HttpPost]
-        public void Post([FromBody] string value)
+        /// <summary>
+        ///     API endpoint for retrieving a booking with a bookingID
+        ///     Checks if the given request is valid and then queries the service
+        ///     If the Booking cannot be found an InvalidBookingException is thrown
+        ///     response code is returned.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>Http response code and json object</returns>
+        [HttpGet]
+        [Route("GetBooking")]
+        public async Task<ActionResult> GetBooking([FromQuery] GetBookingRequest request)
         {
-        }
-
-        // PUT: api/Booking/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/Booking/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            if (request != null)
+            {
+                try
+                {
+                    var booking = await _bookingService.ViewBooking(request);
+                    var json = JsonConvert.SerializeObject(booking);
+                    return Ok(json);
+                }
+                catch (InvalidBookingException e)
+                {
+                    throw e;
+                }
+                
+            }
+            else
+            {
+                return BadRequest("Request is null or empty");
+            }
+            
         }
     }
 }
