8a705b7d7067c9076f4c24eeb2c1a68207934dad
jmshltt <u16003251@tuks.co.za>
2021-07-04 15:23:57 +0200

    Implemented IBookingService.cs

--- backend/backend_api/Services/Booking/BookingService.cs
+++ backend/backend_api/Services/Booking/BookingService.cs
@@ -1,11 +1,12 @@
 ﻿using System.Threading.Tasks;
 using backend_api.Models.Booking.Requests;
+using backend_api.Models.Booking.Responses;
 
 namespace backend_api.Services.Booking
 {
     public class BookingService: IBookingService
     {
-
+        
         public Task<CreateBookingResponse> CreateBooking(CreateBookingRequest request)
         {
             
