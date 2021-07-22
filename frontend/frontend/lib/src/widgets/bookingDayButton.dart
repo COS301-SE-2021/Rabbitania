2ff14acc1151313ed5d8c228dadd06766e3a7cf1
Matt Harty <matthewnharty@gmail.com>
2021-07-22 11:10:51 +0200

    Added Routing to the day page
    - Need to fix overflow issue

--- frontend/frontend/lib/src/widgets/bookingDayButton.dart
+++ frontend/frontend/lib/src/widgets/bookingDayButton.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/src/screens/bookingDayScreen.dart';
 
 class BookingDayButton extends StatefulWidget {
   final displayText;
   BookingDayButton(this.displayText);
-  _bookingDayState createState() => _bookingDayState();
+  _BookingDayState createState() => _BookingDayState();
 }
 
-class _bookingDayState extends State<BookingDayButton> {
+class _BookingDayState extends State<BookingDayButton> {
+  bool _colorChange = false;
+
   @override
   Widget build(context) => TextButton(
         style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(Colors.transparent),
+          backgroundColor: _colorChange
+              ? MaterialStateProperty.all(Colors.transparent)
+              : MaterialStateProperty.all(Color.fromRGBO(33, 33, 33, 1)),
           shape: MaterialStateProperty.all(CircleBorder(
             side: BorderSide(width: 2, color: Color.fromRGBO(171, 255, 79, 1)),
           )),
         ),
-        onPressed: () {},
+        // style: ButtonStyle(
+        //   backgroundColor: MaterialStateProperty.all(Colors.transparent),
+        //   shape: MaterialStateProperty.all(CircleBorder(
+        //     side: BorderSide(width: 2, color: Color.fromRGBO(171, 255, 79, 1)),
+        //   )),
+        // ),
+        onPressed: () => {
+          setState(() => _colorChange = !_colorChange),
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => BookingDayScreen()),
+          )
+        },
         child: Text(
           widget.displayText,
           style: TextStyle(
