57bf0816896b5b1cf8824bd003c34647e83e1d17
Joseph <u16049943@tuks.co.za>
2021-07-25 11:31:15 +0200

    CreateForumThreadResponse Implementation

--- backend/backend_api/Models/Forum/Responses/CreateForumThreadResponse.cs
+++ backend/backend_api/Models/Forum/Responses/CreateForumThreadResponse.cs
@@ -1,7 +1,20 @@
-﻿namespace backend_api.Models.Forum.Responses
+﻿using System.Net;
+
+namespace backend_api.Models.Forum.Responses
 {
     public class CreateForumThreadResponse
     {
-        
+        private HttpStatusCode _response;
+
+        public HttpStatusCode Response
+        {
+            get => _response;
+            set => _response = value;
+        }
+
+        public CreateForumThreadResponse(HttpStatusCode response)
+        {
+            _response = response;
+        }
     }
 }
