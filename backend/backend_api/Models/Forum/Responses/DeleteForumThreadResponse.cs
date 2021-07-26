dd9c4294dc442df10c9af35c991f608c21fa73c8
Joseph <u16049943@tuks.co.za>
2021-07-26 14:01:38 +0200

    Implemented DeleteForumThread Request and Response Models

---
+++ backend/backend_api/Models/Forum/Responses/DeleteForumThreadResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using backend_api.Models.Forum.Requests;
+
+namespace backend_api.Models.Forum.Responses
+{
+    public class DeleteForumThreadResponse
+    {
+        private HttpStatusCode _httpStatusCode;
+
+        public DeleteForumThreadResponse()
+        {
+            
+        }
+
+        public DeleteForumThreadResponse(HttpStatusCode httpStatusCode)
+        {
+            _httpStatusCode = httpStatusCode;
+        }
+
+        public HttpStatusCode HttpStatusCode
+        {
+            get => _httpStatusCode;
+            set => _httpStatusCode = value;
+        }
+    }
+}
