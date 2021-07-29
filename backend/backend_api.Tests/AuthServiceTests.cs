f1702dfa7b540a26a000b276a5c55bc9fa6e808e
jmshltt <u16003251@tuks.co.za>
2021-07-29 16:32:40 +0200

    name fixes

--- backend/backend_api.Tests/AuthServiceTests.cs
+++ backend/backend_api.Tests/AuthServiceTests.cs
@@ -21,7 +21,7 @@ namespace backend_api.Tests
         private readonly AuthService _authService;
         private readonly Mock<IUserRepository> _userRepositoryMock = new Mock<IUserRepository>();
         private readonly ITestOutputHelper outHelper;
-        private readonly User _mockedUser;
+        private readonly Users _mockedUser;
         private readonly UserEmails _mockedEmail;
         private readonly UserService _userService;
 
@@ -32,7 +32,7 @@ namespace backend_api.Tests
             
             this.outHelper = outHelp;
             
-            this._mockedUser = new User(
+            this._mockedUser = new Users(
                 50,
                 "Unit Tests",
                 "0834758854",
@@ -84,7 +84,7 @@ namespace backend_api.Tests
         {
             //Arrange
             var signInRequest = new GoogleSignInRequest(
-                _mockedUser.Name, _mockedEmail.UserEmail, _mockedUser.PhoneNumber, _mockedUser.UserImgUrl
+                _mockedUser.Name, _mockedEmail.UsersEmail, _mockedUser.PhoneNumber, _mockedUser.UserImgUrl
                 );
             
             var request2 = new GoogleSignInRequest(
