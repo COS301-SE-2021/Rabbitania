10e096047d66f493669f7aa4c2b6a818598ad0af
Deannortje <u17326622@tuks.co.za>
2021-07-30 11:57:31 +0200

    Edit to forum latest Thread

--- frontend/frontend/lib/src/widgets/Forum/forumLatestThread.dart
+++ frontend/frontend/lib/src/widgets/Forum/forumLatestThread.dart
@@ -1,32 +1,29 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/src/models/forumModel.dart';
+import 'package:frontend/src/screens/Forum/forumScreen.dart';
 
-class ForumLatestThread extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          ListTile(
-            contentPadding:
-                EdgeInsets.only(bottom: 10.0, top: 10, left: 20, right: 10),
-            title: Container(
-              padding: EdgeInsets.only(bottom: 8),
-              child: Text(
-                "Latest Thread",
-                style: TextStyle(
-                    letterSpacing: 2.0, color: Colors.white, fontSize: 22),
-              ),
-            ),
-            subtitle: Text(
-              "This Thread is the latest",
-              style: TextStyle(color: Colors.white),
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+Widget ForumLatestThread(int forumIdentifier) {
+  futureForumThread = fetchForumThreads(forumIdentifier);
+
+  return Center(
+    child: Column(
+      children: [
+        FutureBuilder<List<ForumThread>>(
+          future: futureForumThread,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text("BABABOOEY");
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            } else {
+              return CircularProgressIndicator(
+                color: Color.fromRGBO(171, 255, 79, 1),
+              );
+            }
+          },
+        ),
+      ],
+    ),
+  );
 }
