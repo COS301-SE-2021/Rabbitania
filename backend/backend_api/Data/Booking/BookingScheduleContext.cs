083dcb300c37049eafbd7130b06310d227fac502
jmshltt <u16003251@tuks.co.za>
2021-07-31 00:36:33 +0200

    Implemented BookingScheduleContext.cs and interface

---
+++ backend/backend_api/Data/Booking/BookingScheduleContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_api.Data.Booking
+{
+    public class BookingScheduleContext: DbContext, IBookingScheduleContext
+    {
+        public BookingScheduleContext()
+        {
+            
+        }
+        public BookingScheduleContext(DbContextOptions<BookingContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Models.Booking.BookingSchedule> BookingSchedules { get; set; }
+        public new async Task<int> SaveChanges()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
