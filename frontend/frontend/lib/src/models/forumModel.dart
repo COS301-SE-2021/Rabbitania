46af392b53b5939cc04c16c439d91f3b778a84ce
Deannortje <u17326622@tuks.co.za>
2021-07-30 11:56:23 +0200

    Added API call for Forum Threads

--- frontend/frontend/lib/src/models/forumModel.dart
+++ frontend/frontend/lib/src/models/forumModel.dart
@@ -2,35 +2,38 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 
-class ForumThreads {
+////////////////////////////////////////////////////////////////
+/// Forum Getting Forums
+///////////////////////////////////////////////////////////////
+class ForumObjs {
   final List<dynamic> forumThreadList;
 
-  ForumThreads({
+  ForumObjs({
     required this.forumThreadList,
   });
 
-  factory ForumThreads.fromJson(Map<String, dynamic> json) {
-    return ForumThreads(
+  factory ForumObjs.fromJson(Map<String, dynamic> json) {
+    return ForumObjs(
       forumThreadList: json["forums"],
     );
   }
 }
 
-class ForumThread {
+class ForumObj {
   final int forumId;
   final String forumTitle;
   final String createdDate;
   final int userId;
 
-  ForumThread({
+  ForumObj({
     required this.forumId,
     required this.forumTitle,
     required this.createdDate,
     required this.userId,
   });
 
-  factory ForumThread.fromJson(Map<String, dynamic> json) {
-    return ForumThread(
+  factory ForumObj.fromJson(Map<String, dynamic> json) {
+    return ForumObj(
       forumId: json['forumId'],
       forumTitle: json['forumTitle'],
       createdDate: json['createdDate'],
@@ -39,7 +42,7 @@ class ForumThread {
   }
 }
 
-Future<List<ForumThread>> fetchForumThread() async {
+Future<List<ForumObj>> fetchForum() async {
   HttpClient client = new HttpClient();
   client.badCertificateCallback =
       ((X509Certificate cert, String host, int port) => true);
@@ -51,6 +54,84 @@ Future<List<ForumThread>> fetchForumThread() async {
   HttpClientResponse response1 = await request.close();
   String reply = await response1.transform(utf8.decoder).join();
 
+  List<dynamic> tList = ForumObjs.fromJson(jsonDecode(reply)).forumThreadList;
+
+  List<ForumObj> threadObj = [];
+
+  for (var t in tList) {
+    threadObj.add(ForumObj.fromJson(t));
+  }
+
+  client.close();
+  return threadObj;
+}
+
+////////////////////////////////////////////////////////////////
+/// Forum Getting Forum Threads
+///////////////////////////////////////////////////////////////
+
+class ForumThreads {
+  final List<dynamic> forumThreadList;
+
+  ForumThreads({
+    required this.forumThreadList,
+  });
+
+  factory ForumThreads.fromJson(Map<String, dynamic> json) {
+    return ForumThreads(
+      forumThreadList: json["forumThreads"],
+    );
+  }
+}
+
+class ForumThread {
+  final int forumThreadId;
+  final String forumThreadTitle;
+  final String createdDate;
+  final String imageURL;
+  final int forumId;
+  final String? forum;
+  final int userId;
+  final String? user;
+
+  ForumThread({
+    required this.forumThreadId,
+    required this.forumThreadTitle,
+    required this.createdDate,
+    required this.imageURL,
+    required this.forumId,
+    required this.forum,
+    required this.userId,
+    required this.user,
+  });
+
+  factory ForumThread.fromJson(Map<String, dynamic> json) {
+    return ForumThread(
+      forumThreadId: json['forumThreadId'],
+      forumThreadTitle: json['forumThreadTitle'],
+      createdDate: json['createdDate'],
+      imageURL: json['imageURL'],
+      forumId: json['forumId'],
+      forum: json['forum'],
+      userId: json['userId'],
+      user: json['user'],
+    );
+  }
+}
+
+Future<List<ForumThread>> fetchForumThreads(int forumIdentifier) async {
+  HttpClient client = new HttpClient();
+  client.badCertificateCallback =
+      ((X509Certificate cert, String host, int port) => true);
+  String url = 'http://10.0.2.2:5000/api/Forum/RetrieveForumThreads?ForumID=' +
+      forumIdentifier.toString();
+
+  HttpClientRequest request = await client.getUrl(Uri.parse(url));
+  request.headers.set('content-type', 'application/json');
+
+  HttpClientResponse response1 = await request.close();
+  String reply = await response1.transform(utf8.decoder).join();
+  print(jsonDecode(reply));
   List<dynamic> tList =
       ForumThreads.fromJson(jsonDecode(reply)).forumThreadList;
 
