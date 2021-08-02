2fe932ce64b785eda0670aecb9b6e71fcdea1886
DeVilliers <u17074292@tuks.co.za>
2021-08-02 11:16:21 +0200

    trying new solution for building listview

--- frontend/frontend/lib/src/provider/booking_provider.dart
+++ frontend/frontend/lib/src/provider/booking_provider.dart
@@ -18,14 +18,15 @@ class BookingProvider {
       List<dynamic> jsonResponse = jsonDecode(response.body);
       print(jsonResponse);
       List<ViewBookingModel> objList = [];
-      for (var t in jsonResponse) {
-        print(t);
+      print(jsonResponse);
+      jsonResponse.forEach((element) {
+        print(element);
         objList.add(
           ViewBookingModel.fromJson(
-            jsonDecode(t),
+            jsonDecode(element),
           ),
         );
-      }
+      });
       return ViewBookingListModel(objList);
     } else {
       throw Exception(
