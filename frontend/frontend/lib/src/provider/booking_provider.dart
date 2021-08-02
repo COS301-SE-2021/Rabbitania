f35e4ca08c39ff22adac6b292b8d374f7b7633aa
DeVilliers <u17074292@tuks.co.za>
2021-08-02 11:04:54 +0200

    working on fixing http request

--- frontend/frontend/lib/src/provider/booking_provider.dart
+++ frontend/frontend/lib/src/provider/booking_provider.dart
@@ -15,16 +15,17 @@ class BookingProvider {
     );
     if (response.statusCode == 200) {
       print(response.body);
-      var jsonResponse = jsonDecode(response.body).toList();
+      List<dynamic> jsonResponse = jsonDecode(response.body);
       print(jsonResponse);
       List<ViewBookingModel> objList = [];
-      jsonResponse.foreach((element) => {
-            objList.add(
-              ViewBookingModel.fromJson(
-                jsonDecode(element),
-              ),
-            ),
-          });
+      for (var t in jsonResponse) {
+        print(t);
+        objList.add(
+          ViewBookingModel.fromJson(
+            jsonDecode(t),
+          ),
+        );
+      }
       return ViewBookingListModel(objList);
     } else {
       throw Exception(
