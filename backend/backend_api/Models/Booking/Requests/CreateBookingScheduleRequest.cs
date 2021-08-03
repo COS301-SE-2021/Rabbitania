7b20ab2c5487222f3c9c28826bcd74ea7901e66e
jmshltt <u16003251@tuks.co.za>
2021-08-03 23:10:33 +0200

    CreateBookingScheduleRequest.cs completed

--- backend/backend_api/Models/Booking/Requests/CreateBookingScheduleRequest.cs
+++ backend/backend_api/Models/Booking/Requests/CreateBookingScheduleRequest.cs
@@ -1,7 +1,40 @@
-﻿namespace backend_api.Models.Booking.Requests
+﻿using backend_api.Models.User;
+
+namespace backend_api.Models.Booking.Requests
 {
     public class CreateBookingScheduleRequest
     {
-        
+        private OfficeLocation office;
+        private string timeSlot;
+        private int availability;
+
+        public CreateBookingScheduleRequest(OfficeLocation office, string timeSlot, int availability)
+        {
+            this.office = office;
+            this.timeSlot = timeSlot;
+            this.availability = availability;
+        }
+
+        public CreateBookingScheduleRequest()
+        {
+        }
+
+        public OfficeLocation Office
+        {
+            get => office;
+            set => office = value;
+        }
+
+        public string TimeSlot
+        {
+            get => timeSlot;
+            set => timeSlot = value;
+        }
+
+        public int Availability
+        {
+            get => availability;
+            set => availability = value;
+        }
     }
 }
