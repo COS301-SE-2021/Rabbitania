c7e17be1b18a3f7af43eeddd8e21a822f1b24102
jmshltt <u16003251@tuks.co.za>
2021-08-03 22:31:46 +0200

    CancelBookingScheduleRequest.cs completed

--- backend/backend_api/Models/Booking/Requests/CancelBookingScheduleRequest.cs
+++ backend/backend_api/Models/Booking/Requests/CancelBookingScheduleRequest.cs
@@ -1,7 +1,32 @@
-﻿namespace backend_api.Models.Booking.Requests
+﻿using backend_api.Models.User;
+
+namespace backend_api.Models.Booking.Requests
 {
     public class CancelBookingScheduleRequest
     {
-        
+        private string timeSlot;
+        private OfficeLocation office;
+
+        public CancelBookingScheduleRequest(string timeSlot, OfficeLocation office)
+        {
+            this.timeSlot = timeSlot;
+            this.office = office;
+        }
+
+        public CancelBookingScheduleRequest()
+        {
+        }
+
+        public string TimeSlot
+        {
+            get => timeSlot;
+            set => timeSlot = value;
+        }
+
+        public OfficeLocation Office
+        {
+            get => office;
+            set => office = value;
+        }
     }
 }
