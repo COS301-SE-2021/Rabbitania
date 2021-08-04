3f5a30107b2ea06f54fa753c426313d8b79cc049
jmshltt <u16003251@tuks.co.za>
2021-08-04 18:06:08 +0200

    Implemented IBookingScheduleService.cs

---
+++ backend/backend_api/Services/Booking/BookingScheduleService.cs
@@ -0,0 +1,7 @@
+﻿namespace backend_api.Services.Booking
+{
+    public class BookingScheduleService
+    {
+        
+    }
+}
---
+++ backend/backend_api/Services/Booking/IBookingScheduleService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using backend_api.Models.Booking.Requests;
+using backend_api.Models.Booking.Responses;
+
+namespace backend_api.Services.Booking
+{
+    public interface IBookingScheduleService
+    {
+        Task<CreateBookingScheduleResponse> CreateBookingSchedule(CreateBookingScheduleRequest request);
+        
+        Task<CancelBookingScheduleResponse> CancelBookingSchedule(CancelBookingScheduleRequest request);
+        
+        Task<UpdateBookingScheduleResponse> UpdateBookingSchedule(UpdateBookingScheduleRequest request);
+
+        Task<GetBookingScheduleResponse> ViewBookingSchedule(GetBookingScheduleRequest request);
+
+        Task<GetAllBookingSchedulesResponse> ViewAllBookingSchedules(GetAllBookingSchedulesRequest request);
+
+    }
+}
