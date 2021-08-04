6f08ada049223f70709a7b5746e5fd976a4ac0d3
jmshltt <u16003251@tuks.co.za>
2021-08-04 10:42:00 +0200

    Booking schedule repository interface

--- backend/backend_api/Data/Booking/IBookingScheduleRepository.cs
+++ backend/backend_api/Data/Booking/IBookingScheduleRepository.cs
@@ -1,7 +1,17 @@
-﻿namespace backend_api.Data.Booking
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using backend_api.Models.Booking;
+using backend_api.Models.Booking.Requests;
+using backend_api.Models.Booking.Responses;
+
+namespace backend_api.Data.Booking
 {
     public interface IBookingScheduleRepository
     {
-        
+        Task<GetBookingScheduleResponse> GetBookingSchedule(GetBookingRequest request);
+        Task<GetAllBookingSchedulesResponse> GetAllBookingSchedules(GetAllBookingSchedulesRequest request);
+        Task<CancelBookingScheduleResponse> CancelBookingSchedule(CancelBookingScheduleRequest request);
+        Task<UpdateBookingScheduleResponse> UpdateBookingSchedule(UpdateBookingScheduleRequest request);
+        Task<CreateBookingScheduleResponse> CreateBookingSchedule(CreateBookingScheduleRequest request);
     }
 }
