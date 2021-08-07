d87824c6dee3e0180e462a8204692521163f7c8e
jmshltt <u16003251@tuks.co.za>
2021-08-07 09:51:17 +0200

    Created Check Availability endpoint

---
+++ backend/backend_api/Models/Booking/Requests/CheckScheduleAvailabilityRequest.cs
@@ -0,0 +1,28 @@
+﻿using backend_api.Models.User;
+
+namespace backend_api.Models.Booking.Requests
+{
+    public class CheckScheduleAvailabilityRequest
+    {
+        private string timeSlot;
+        private OfficeLocation office;
+
+        public CheckScheduleAvailabilityRequest(string timeSlot, OfficeLocation office)
+        {
+            this.timeSlot = timeSlot;
+            this.office = office;
+        }
+
+        public string TimeSlot
+        {
+            get => timeSlot;
+            set => timeSlot = value;
+        }
+
+        public OfficeLocation Office
+        {
+            get => office;
+            set => office = value;
+        }
+    }
+}
---
+++ backend/backend_api/Models/Booking/Responses/CheckScheduleAvailabilityResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace backend_api.Models.Booking.Responses
+{
+    public class CheckScheduleAvailabilityResponse
+    {
+        private bool successful;
+
+        public CheckScheduleAvailabilityResponse(bool successful)
+        {
+            this.successful = successful;
+        }
+
+        public CheckScheduleAvailabilityResponse()
+        {
+        }
+
+        public bool Successful
+        {
+            get => successful;
+            set => successful = value;
+        }
+    }
+}
