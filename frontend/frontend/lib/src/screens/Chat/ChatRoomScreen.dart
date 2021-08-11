98a3ca9f7524a495057b025a666b55c7e335d40b
jmshltt <u16003251@tuks.co.za>
2021-08-11 23:31:58 +0200

    Chat client functionality

--- frontend/frontend/lib/src/screens/Chat/ChatRoomScreen.dart
+++ frontend/frontend/lib/src/screens/Chat/ChatRoomScreen.dart
@@ -1,182 +1,80 @@
+import 'package:signalr_core/signalr_core.dart';
+
 import '../../../main.dart';
 import '../../widgets/Chat/ChatModelPropertyWidgetBuilder.dart';
 import '../../models/Chat/ChatPageModel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class ChatPage extends StatelessWidget {
-// Properties
-
-// Methods
-
+class ChatPage extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    final vm = new ChatPageViewModel();
-    return ChatPageViewModelProvider(
-        viewModel: vm,
-        childBuilder: (ctx) {
-          return Scaffold(
-              appBar: AppBar(title: Text("Server at: $ServerURL")),
-              resizeToAvoidBottomInset: false,
-              body: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: <Widget>[
-                    MessageComposeView(),
-                    Divider(),
-                    Expanded(
-                      child: MessageChatView(),
-                    ),
-                  ],
-                ),
-              ));
-        });
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
   }
+
 }
 
-class MessageChatView extends StatelessWidget {
-  // Properites
+class ChatPageState extends State<ChatPage> {
+  final serverURLEmulator = "https://10.0.2.2:5001/api/ChatHub";
+  final serverURL = "https://localhost:5001/api/ChatHub";
+  final userID = 0;
+  HubConnection? _hubConnection;
 
-  // Methods
+  @override
+  void initState() {
+    super.initState();
+    initSignalR();
+    var user = 0;
+  }
+
+  void initSignalR() {
+    _hubConnection = HubConnectionBuilder().withUrl(serverURLEmulator).build();
+    _hubConnection!.onclose((exception) {
+      print('Connection closed');
+    });
+    _hubConnection!.on("ReceiveMessage", sendMessage());
+  }
+
+  sendMessage() {
+    setState(() {
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final vm = ChatPageViewModelProvider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chat"),
+      ),
+      body: Center(
+        child: Column(
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            Text(
-              "Messages:",
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-            ViewModelPropertyWidgetBuilder<bool>(
-                viewModel: vm,
-                propertyName: ChatPageViewModel.connectionIsOpenPropName,
-                builder: (context, snapshot) {
-                  return Text(
-                      vm.connectionIsOpen ? "Connected" : "Disconnected");
-                }),
-          ],
         ),
-        Expanded(
-            child: ViewModelPropertyWidgetBuilder<ChatMessage>(
-              viewModel: vm,
-              propertyName: ChatPageViewModel.chatMessagesPropName,
-              builder: (context, snapshot) {
-                return ListView.builder(
-                    itemCount: vm.chatMessages.length,
-                    itemBuilder: (BuildContext ctx, int index) =>
-                        _createMessageItemView(vm.chatMessages[index]));
-              },
-            )),
-      ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async{
+          _hubConnection!.state == HubConnectionState.disconnected ?
+              await _hubConnection!.start()
+              :
+              await _hubConnection!.stop();
+        },
+        tooltip: 'Start/Stop',
+      ),
     );
   }
 
-  Widget _createMessageItemView(ChatMessage message) {
-    return Column(children: <Widget>[
-      ListTile(
-          leading: Text("${message.senderName} :"),
-          title: Text(message.message)),
-      Divider(),
-    ]);
+  connect() {
+    if (_hubConnection!.state == HubConnectionState.connected)
+      _hubConnection!.invoke("SendMessage", args:<Object>[
+        userID,
+        "Message"
+      ]);
+    setState(() {
+
+    });
   }
 }
 
-class MessageComposeView extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _MessageComposeViewState();
-}
 
-class _MessageComposeViewState extends State<MessageComposeView> {
-  // Properties
-  final TextEditingController _messageTextController = TextEditingController();
-  final TextEditingController _userNameController = TextEditingController();
-
-  // Methods
-
-  @override
-  void dispose() {
-    // Clean up the controller when the Widget is disposed
-    _messageTextController.dispose();
-    _userNameController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final vm = ChatPageViewModelProvider.of(context);
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        ViewModelPropertyWidgetBuilder<String>(
-            viewModel: vm,
-            propertyName: ChatPageViewModel.userNamePropName,
-            builder: (context, snapshot) {
-              return ElevatedButton(
-                child: Text("${vm.userName} says"),
-                onPressed: _handleUpdateUserName,
-              );
-            }),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Flexible(
-              child: TextField(
-                controller: _messageTextController,
-                decoration: InputDecoration(
-                    labelText: 'Your message:', hintText: 'eg. Hi there!'),
-              ),
-            ),
-            ElevatedButton(
-              child: Text("send"),
-              onPressed: () => vm.sendChatMessage(_messageTextController.text),
-            )
-          ],
-        )
-      ],
-    );
-  }
-
-  void _handleUpdateUserName() async {
-    final vm = ChatPageViewModelProvider.of(context);
-    await showDialog<String>(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            contentPadding: const EdgeInsets.all(16.0),
-            content: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    controller: _userNameController,
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        labelText: 'Your Name', hintText: 'eg. John Smith'),
-                  ),
-                )
-              ],
-            ),
-            actions: <Widget>[
-              TextButton(
-                  child: const Text('CANCEL'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-              TextButton(
-                  child: const Text('OK'),
-                  onPressed: () {
-                    vm.userName = _userNameController.text;
-                    Navigator.pop(context);
-                  })
-            ],
-          );
-        });
-  }
-}
