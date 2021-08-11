751b1fcd2e06143d2a9b4c8e7eac6228c1ba5363
Matt Harty <matthewnharty@gmail.com>
2021-08-11 16:27:35 +0200

    Fixed the UI for the View Booking

--- frontend/frontend/lib/src/provider/booking_provider.dart
+++ frontend/frontend/lib/src/provider/booking_provider.dart
@@ -8,9 +8,8 @@ import 'package:http/http.dart' as http;
 class BookingProvider {
   // GET ALL
   Future<List<ViewBookingModel>> fetchBookingsAsync() async {
-    //UserProvider userProvider = new UserProvider();
     final response = await http.get(
-      Uri.parse('https://10.0.2.2:5001/api/Booking/GetBookings?UserId=8'),
+      Uri.parse('https://10.0.2.2:5001/api/Booking/GetBookings?UserId=1'),
     );
 
     if (response.statusCode == 200) {
--- frontend/frontend/lib/src/widgets/Booking/viewBookingCard.dart
+++ frontend/frontend/lib/src/widgets/Booking/viewBookingCard.dart
@@ -4,8 +4,14 @@ import 'package:frontend/src/models/Booking/bookingModel.dart';
 import 'package:frontend/src/provider/booking_provider.dart';
 import 'package:frontend/src/widgets/Booking/cancelBookingButton.dart';
 
-class BookingListVew extends StatelessWidget {
+class BookingListView extends StatefulWidget {
+  @override
+  _BookingListView createState() => _BookingListView();
+}
+
+class _BookingListView extends State<BookingListView> {
   final _bookingProvider = new BookingProvider();
+  final items = List<String>.generate(20, (i) => "Item ${i + 1}");
 
   @override
   Widget build(BuildContext context) {
@@ -27,50 +33,225 @@ class BookingListVew extends StatelessWidget {
     );
   }
 
-  /*
-  final int id;
-  final String bookingDate;
-  final String timeSlot;
-  final int office;
-  final int userId;
-  */
   ListView _bookingCardListView(data) {
+    BookingProvider bookingProvider = new BookingProvider();
+
     return ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (context, index) {
-          return _bookingCardTile(
-              data[index].timeSlot,
-              data[index].bookingDate,
-              data[index].office,
-              data[index].id,
-              Icons.bookmark_added_outlined);
-        });
+      itemCount: data.length,
+      // itemBuilder: (context, index) {
+      //   return CustomListItem(
+      //     Icons.bookmark_added_outlined,
+      //     data[index].timeSlot,
+      //     data[index].office,
+      //     data[index].bookingDate,
+      //     data[index].id,
+      //   );
+      // },
+      itemBuilder: (context, index) {
+        return Dismissible(
+          key: Key(
+            data[index].id.toString(),
+          ),
+          onDismissed: (direction) {
+            bookingProvider.deleteBookingAsync(data[index].id);
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  'Booking Successfully Deleted',
+                  style: TextStyle(
+                    color: Color.fromRGBO(171, 255, 79, 1),
+                    fontSize: 20,
+                  ),
+                ),
+                duration: const Duration(milliseconds: 5000),
+                width: 300.0,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                ),
+                behavior: SnackBarBehavior.floating,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+              ),
+            );
+          },
+          background: Container(
+            color: Colors.red,
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Container(
+                margin: const EdgeInsets.only(right: 15),
+                child: Text(
+                  'DELETE \nBOOKING',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Color.fromRGBO(33, 33, 33, 1),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          child: CustomListItem(
+            Icons.bookmark_added_outlined,
+            data[index].timeSlot,
+            data[index].office,
+            data[index].bookingDate,
+            data[index].id,
+          ),
+        );
+      },
+    );
   }
 
-  ListTile _bookingCardTile(String title, String subtitle, int subsubtitle,
-          int id, IconData icon) =>
-      ListTile(
-        isThreeLine: true,
-        title: Text(
-          title,
-          style: TextStyle(
-            fontWeight: FontWeight.w500,
-            color: Color.fromRGBO(171, 255, 79, 1),
-            fontSize: 25,
-          ),
-        ),
-        subtitle: Text(
-          'Time Slot: ' + subtitle.toString(),
-          style: TextStyle(
-            color: Color.fromRGBO(171, 255, 79, 1),
-            fontSize: 20,
-          ),
-        ),
-        trailing: CancelBookingButton(id),
-        leading: Icon(
-          icon,
-          size: 35,
-          color: Color.fromRGBO(171, 255, 79, 1),
-        ),
-      );
+//   ListTile _bookingCardTile(String title, String subtitle, int subsubtitle,
+//           int id, IconData icon) =>
+//       ListTile(
+//         isThreeLine: true,
+//         title: Text(
+//           title,
+//           style: TextStyle(
+//             fontWeight: FontWeight.w500,
+//             color: Color.fromRGBO(171, 255, 79, 1),
+//             fontSize: 25,
+//           ),
+//         ),
+//         subtitle: Text(
+//           'Time Slot: ' + subtitle.toString(),
+//           style: TextStyle(
+//             color: Color.fromRGBO(171, 255, 79, 1),
+//             fontSize: 20,
+//           ),
+//         ),
+//         trailing: CancelBookingButton(id),
+//         leading: Icon(
+//           icon,
+//           size: 35,
+//           color: Color.fromRGBO(171, 255, 79, 1),
+//         ),
+//       );
 }
+
+class CustomListItem extends StatelessWidget {
+  const CustomListItem(
+      this.icon, this.slot, this.office, this.bookingDate, this.id);
+
+  final IconData icon;
+  final String slot;
+  final int office;
+  final String bookingDate;
+  final int id;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Icon(
+              icon,
+              size: 45,
+              color: Color.fromRGBO(171, 255, 79, 1),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: _BookingInformation(
+              day: slot,
+              slot: slot,
+              office: office,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BookingInformation extends StatelessWidget {
+  const _BookingInformation({
+    Key? key,
+    required this.day,
+    required this.slot,
+    required this.office,
+  }) : super(key: key);
+
+  final String day;
+  final String slot;
+  final int office;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            day,
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              color: Color.fromRGBO(171, 255, 79, 1),
+              fontSize: 24,
+            ),
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 2.0)),
+          Text(
+            'Slot: ' + slot.toString(),
+            style: TextStyle(
+              color: Color.fromRGBO(171, 255, 79, 1),
+              fontSize: 20,
+            ),
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 1.0)),
+          Text(
+            'Office: ' + office.toString(),
+            style: TextStyle(
+              color: Color.fromRGBO(171, 255, 79, 1),
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//   ListView _bookingCardListView(data) {
+//     return ListView.builder(
+//         itemCount: data.length,
+//         itemBuilder: (context, index) {
+//           return _bookingCardTile(
+//               data[index].timeSlot,
+//               data[index].bookingDate,
+//               data[index].office,
+//               data[index].id,
+//               Icons.bookmark_added_outlined);
+//         });
+//   }
+
+/// This is the stateless widget that the main application instantiates.
+// class MyStatelessWidget extends StatelessWidget {
+//   const MyStatelessWidget({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//       itemCount: items.length,
+//       itemBuilder: (context, index) {
+//         CustomListItem(
+//           user: 'Morning',
+//           viewCount: 'Pretoria',
+//           starting: Icon(
+//             Icons.bookmark_added_outlined,
+//             size: 45,
+//             color: Color.fromRGBO(171, 255, 79, 1),
+//           ),
+//           title: 'Monday',
+//         );
+//       },
+//     );
+//   }
+// }
