03638568388ef52651f3437a03a922450a1ab6da
DeVilliers <u17074292@tuks.co.za>
2021-08-11 11:18:57 +0200

    alterted helper function and storage call for user

--- frontend/frontend/lib/src/helper/Login/loginHelper.dart
+++ frontend/frontend/lib/src/helper/Login/loginHelper.dart
@@ -4,6 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 //helper class for all login related functionality
 class LoginHelper {
   //function used to save userID to disk
+  //function is async so must use callback when using
   void setUserID(userID) async {
     //get instance of sharedpreferences obj
     final prefs = await SharedPreferences.getInstance();
@@ -12,12 +13,14 @@ class LoginHelper {
   }
 
   //funtion used to retrieve stored userID
+  //function is async so must use callback when using
   getUserID() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getInt('userID');
   }
 
   //clear all user specific data from disk
+  //function is async so must use callback when using
   clearPersitantUserData() async {
     final prefs = await SharedPreferences.getInstance();
     prefs.remove('UserID');
