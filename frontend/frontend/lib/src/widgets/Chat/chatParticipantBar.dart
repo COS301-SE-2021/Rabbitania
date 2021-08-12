b4dd3bac705171e1c53129064f280154f0aa953a
DeVilliers <u17074292@tuks.co.za>
2021-08-12 15:00:42 +0200

    added chat participant bar widget

---
+++ frontend/frontend/lib/src/widgets/Chat/chatParticipantBar.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/src/models/util_model.dart';
+import 'package:frontend/src/provider/user_provider.dart';
+import 'package:frontend/src/widgets/Profile/profile_picture_widget.dart';
+
+//Widget used to display information about other chat participant
+class ChatParticipantBar extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _chatParticipantBar();
+  }
+}
+
+class _chatParticipantBar extends State<ChatParticipantBar> {
+  //provider class for getting basic user information
+  UtilModel utilModel = UtilModel();
+  UserProvider userProvider = UserProvider(FirebaseAuth.instance.currentUser);
+  @override
+  Widget build(BuildContext context) => Row(
+        children: <Widget>[
+          //profile picture container
+          Expanded(
+            child: Container(
+              child: ProfilePicture(20),
+            ),
+          ),
+
+          Expanded(
+            flex: 7,
+            child: Container(
+              child: Text(
+                userProvider.getUserDisplayName(),
+                style: TextStyle(
+                  color: utilModel.greenColor,
+                ),
+              ),
+            ),
+          )
+        ],
+      );
+}
