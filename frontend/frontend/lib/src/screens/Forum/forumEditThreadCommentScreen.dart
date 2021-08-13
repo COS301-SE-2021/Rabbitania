5b369b037b81a4d0487caa1f8627e1ab6f5ac9d5
Joseph <u16049943@tuks.co.za>
2021-08-13 09:17:46 +0200

    Implemented some corrections to user and forum

--- frontend/frontend/lib/src/screens/Forum/forumEditThreadCommentScreen.dart
+++ frontend/frontend/lib/src/screens/Forum/forumEditThreadCommentScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/src/models/Forum/forumModel.dart';
 import 'package:frontend/src/models/util_model.dart';
 import 'package:frontend/src/provider/forum_provider.dart';
 import 'package:frontend/src/screens/Forum/forumThreadScreen.dart';
@@ -58,7 +59,8 @@ class _ForumEditThreadCommentScreen
                           ),
                           tooltip: 'Continue',
                           onPressed: () async {
-                            UtilModel.route(() => ForumThreadScreen(), context);
+                            UtilModel.route(
+                                () => ForumCommentScreen(), context);
                           },
                         ),
                       ],
