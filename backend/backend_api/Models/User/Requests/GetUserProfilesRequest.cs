2e374c7407da2cebef2b860c7a5eb2165a81b209
Joseph <u16049943@tuks.co.za>
2021-08-13 10:19:30 +0200

    Request and response models for GetUserProfiles

---
+++ backend/backend_api/Models/User/Requests/GetUserProfilesRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace backend_api.Models.User.Requests
+{
+    public class GetUserProfilesRequest
+    {
+        public GetUserProfilesRequest()
+        {
+            
+        }
+    }
+}
