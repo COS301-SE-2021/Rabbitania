17226d58a23498e52401cb44c3a65f0f92144e95
DeVilliers <u17074292@tuks.co.za>
2021-08-15 00:00:52 +0200

    edited chatRoomScreen

--- frontend/frontend/lib/src/screens/Chat/ChatRoomScreen.dart
+++ frontend/frontend/lib/src/screens/Chat/ChatRoomScreen.dart
@@ -1,190 +1,95 @@
-import 'dart:async';
-
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:frontend/src/helper/Chat/SignalRHelper.dart';
-import 'package:frontend/src/helper/Chat/chatHelper.dart';
-import 'package:frontend/src/models/Chat/ChatMessageModel.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/src/helper/Chat/fireStoreHelper.dart';
 import 'package:frontend/src/models/util_model.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:frontend/src/widgets/Chat/chatMessageReceiver.dart';
 import 'package:frontend/src/widgets/Chat/chatMessageSender.dart';
-import 'package:frontend/src/widgets/Chat/chatParticipantBar.dart';
 import 'package:frontend/src/widgets/Chat/chatSendMessageBar.dart';
-import 'package:signalr_core/signalr_core.dart';
-import '../../../main.dart';
-import '../../widgets/Chat/ChatModelPropertyWidgetBuilder.dart';
-import '../../models/Chat/ChatPageModel.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class ChatPage extends StatefulWidget {
+class ChatRoomScreen extends StatefulWidget {
+  final idUser;
+  //1== runtimeTerrors , 2==retard, 3==diff , 4==matt
+  final int myId = 3;
+  //i am currently retard
+  // final int myId = 2
+  ChatRoomScreen(this.idUser);
   @override
-  State<StatefulWidget> createState() {
-    return ChatPageState();
-  }
+  State<StatefulWidget> createState() => _chatRoomScreenState();
 }
 
-class ChatPageState extends State<ChatPage> {
-  final serverURLEmulator = "https://10.0.2.2:5001/api/ChatHub";
-  final serverURL = "https://localhost:5001/api/ChatHub";
-  final userID = 0;
-  HubConnection? _hubConnection;
+class _chatRoomScreenState extends State<ChatRoomScreen> {
+  final fireStoreHelper = FireStoreHelper();
   final utilModel = UtilModel();
-  final signalRHelper = SignalRHelper();
-  final chatHelper = ChatHelper();
-  final messageController = TextEditingController();
 
-  @override
-  void initState() {
-    super.initState();
-    initSignalR();
-    var user = 0;
-  }
-
-  void initSignalR() {
-    //used to start connection with signalR serverside
-    signalRHelper.initiateConnection(context);
-    _hubConnection = HubConnectionBuilder().withUrl(serverURLEmulator).build();
-    _hubConnection!.onclose((exception) {
-      print('Connection closed');
-    });
-    //_hubConnection!.on("ReceiveMessage", sendMessage());
-  }
+  initState() {}
 
   @override
   Widget build(BuildContext context) {
+    //TODO: must filter to only see messages sent to current user
     return Scaffold(
       backgroundColor: utilModel.greyColor,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: utilModel.greyColor,
-        title: ChatParticipantBar(),
-      ),
-      body: Center(
-        child: FutureBuilder(
-          future: signalRHelper.getMessagesList(),
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            List<Widget> children = [];
-            if (snapshot.hasData) {
-              if (snapshot.data.length == 0) {
-                children = [
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: Text(
-                        'You have no chat history',
-                        style: TextStyle(
-                          fontSize: 30,
-                          color: utilModel.greenColor,
-                        ),
-                      ),
-                    ),
-                  )
-                ];
-              } else {
-                snapshot.data.forEach((current) {
-                  if (current.messageType == 'Sender') {
-                    children.add(
-                      ChatMessageSender(textSentValue: current.messageContent),
-                    );
-                  } else {
-                    children.add(
-                      ChatMessageReceiver(
-                          textSentValue: current.messageContent),
-                    );
-                  }
-                });
-              }
-            } else if (snapshot.hasError) {
-              children = [
-                Icon(
-                  Icons.error_outline,
-                  color: Colors.red,
-                  size: 60,
-                ),
-              ];
-            } else {
-              children = [
-                Center(
-                  child: Stack(children: <Widget>[
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height,
-                      decoration: new BoxDecoration(
-                        color: Color.fromRGBO(33, 33, 33, 1),
-                      ),
-                      child: Center(
-                        widthFactor: 0.5,
-                        heightFactor: 0.5,
-                        child: CircularProgressIndicator(
-                          color: Color.fromRGBO(171, 255, 79, 1),
-                        ),
-                      ),
-                    ),
-                  ]),
-                ),
-              ];
-            }
-            return ListView(shrinkWrap: true, children: [
-              Column(children: children),
-            ]);
-          },
-        ),
-      ),
-      bottomNavigationBar: Container(
-        child: SafeArea(
-          minimum: EdgeInsets.all(10),
-          child: Row(
-            children: <Widget>[
+      body: Stack(
+        children: [
+          SvgPicture.string(
+            utilModel.svg_background,
+            fit: BoxFit.contain,
+          ),
+          Column(
+            children: [
               Expanded(
-                flex: 8,
-                child: TextField(
-                  controller: messageController,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                  cursorColor: utilModel.greenColor,
-                  textAlign: TextAlign.start,
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: utilModel.greenColor, width: 2.0),
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    fillColor: Colors.transparent,
-                    focusColor: utilModel.greenColor,
-                    hoverColor: utilModel.greenColor,
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: FloatingActionButton(
-                  backgroundColor: utilModel.greenColor,
-                  child: Icon(
-                    FontAwesomeIcons.paperPlane,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {
-                    signalRHelper.sendMessage(
-                        FirebaseAuth.instance.currentUser!.displayName,
-                        messageController.text);
+                flex: 6,
+                child: StreamBuilder(
+                  stream: fireStoreHelper.getChat(widget.idUser, widget.myId),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    print(widget.idUser);
 
-                    setState(() {});
+                    List<Widget> children = [];
+                    if (snapshot.hasData) {
+                      //must loop through messages return object to filter throug
+                      if (snapshot.data.docs.length == 0) {
+                        children.add(
+                          Center(
+                            child: Text(
+                              "You have no chat history with this user. Why don't you say hi",
+                              style: TextStyle(
+                                color: utilModel.greenColor,
+                                fontSize: 25,
+                              ),
+                            ),
+                          ),
+                        );
+                      } else {
+                        for (int i = 0; i < snapshot.data.docs.length; i++) {
+                          //messages correspond to current user and selected user
+
+                          if (snapshot.data.docs[i]['uid'] == widget.myId) {
+                            children.add(ChatMessageSender(
+                                textSentValue: snapshot.data.docs[i]
+                                    ['message']));
+                          } else if (snapshot.data.docs[i]['uid'] ==
+                              widget.idUser) {
+                            children.add(ChatMessageReceiver(
+                                textSentValue: snapshot.data.docs[i]
+                                    ['message']));
+                          }
+                        }
+                      }
+                    } else {
+                      children.add(CircularProgressIndicator());
+                    }
+
+                    //participantID matches clicked on id, means other persons message
+
+                    return ListView(shrinkWrap: true, children: children);
                   },
                 ),
               ),
             ],
           ),
-        ),
+        ],
       ),
+      bottomNavigationBar: ChatSendMessageBar(widget.idUser, widget.myId),
     );
-  }
-
-  connect() {
-    if (_hubConnection!.state == HubConnectionState.connected)
-      _hubConnection!.invoke("SendMessage", args: <Object>[userID, "Message"]);
-    setState(() {});
   }
 }
