8c36993254baf3f0847def18e7586dbaccef02b6
Matt Harty <matthewnharty@gmail.com>
2021-08-16 11:10:08 +0200

    Changed backend deseriazables as returns

--- backend/backend_api/Controllers/Booking/BookingController.cs
+++ backend/backend_api/Controllers/Booking/BookingController.cs
@@ -76,8 +76,7 @@ namespace backend_api.Controllers.Booking
                 try
                 {
                     var booking = await _bookingService.ViewBooking(request);
-                    var json = JsonConvert.SerializeObject(booking);
-                    return Ok(json);
+                    return Ok(booking);
                 }
                 catch (InvalidBookingException e)
                 {
