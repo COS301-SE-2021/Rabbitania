b7c7e06242d42e9826725a6df725e89543a4efad
jmshltt <u16003251@tuks.co.za>
2021-08-19 18:57:29 +0200

    Env variable testing

--- backend/backend_api.Tests/Auth/IntegrationTests/AuthServiceTest.cs
+++ backend/backend_api.Tests/Auth/IntegrationTests/AuthServiceTest.cs
@@ -29,8 +29,8 @@ namespace backend_api.Tests.Auth.IntegrationTests
         {
             var serviceProvider = new ServiceCollection().AddEntityFrameworkNpgsql().BuildServiceProvider();
             var builder = new DbContextOptionsBuilder<UserContext>();
-            var env = Environment.GetEnvironmentVariable("CONN_STRING");
-            builder.UseNpgsql(env)
+            var env = Environment.GetEnvironmentVariable("$CONN_STRING");
+            builder.UseNpgsql(env.ToString())
                 .UseInternalServiceProvider(serviceProvider);
 
             _userContext = new UserContext(builder.Options);
