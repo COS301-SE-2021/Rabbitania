e64c1a7f1ce36a12de0a78242329fd8f116c6bfc
jmshltt <u16003251@tuks.co.za>
2021-08-26 15:38:47 +0200

    Completed documentation for IChatService.cs

--- backend/backend_api/Services/Chat/IChatService.cs
+++ backend/backend_api/Services/Chat/IChatService.cs
@@ -2,6 +2,14 @@
 {
     public interface IChatService
     {
+        /// <summary>
+        ///     Encrypts the Agora AppID using AES 128 bit
+        ///     encryption. The key is stored as a secret, as
+        ///     well as the AppID. This can later be changed
+        ///     to encrypt any supplied text, specifically tokens
+        ///     that have been generated (Agora or JWT).
+        /// </summary>
+        /// <returns> string containing the encrypted text </returns>
         string Encrypt();
     }
 }
