d2035a9171a8f958cf04307a826e7bf57e5a4066
Deannortje <u17326622@tuks.co.za>
2021-08-31 19:05:53 +0200

    Continued work on the ai-planner

--- website/src/app/ai-planner/ai-planner.component.ts
+++ website/src/app/ai-planner/ai-planner.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {CdkDragEnd, CdkDragMove, CdkDragStart, DragDropModule} from '@angular/cdk/drag-drop';
-
+import {MovableNodes} from '../services/ai-planner/movable-nodes';
+import dummyData from '../../test_variables/dummy_node_json.json'
 @Component({
   selector: 'app-ai-planner',
   templateUrl: './ai-planner.component.html',
@@ -8,33 +8,33 @@ import {CdkDragEnd, CdkDragMove, CdkDragStart, DragDropModule} from '@angular/cd
 })
 export class AIPlannerComponent implements OnInit {
 
-  constructor() { }
+  
+  public nodeList:{deskNumber:string, x:string,y:string}[] = dummyData;
 
   ngOnInit(): void {
     console.log("Starting");
+    //APi call to get nodes pos/name/details
+    this.getDummyNodes();
   }
 
-  state = '';
-  position = '';
 
-  
-  dragStarted(event: CdkDragStart) {
-    this.state = 'dragStarted';
+  // newMN1 = new MovableNodes(1,0,0);
+  // newMN2 = new MovableNodes(2,977,0);
+  // newMN3 = new MovableNodes(3,0,777);
+  // newMN4 = new MovableNodes(4,977,777);
+  // newMN5 = new MovableNodes(5,472,372);
+
+  nodes: MovableNodes[] = [];
+
+  getDummyNodes()
+  {
+    for(var i =0; i< dummyData.length;i++)
+    {
+      //console.log(dummyData[i].deskNumber);
+      this.nodes.push(new MovableNodes(Number(dummyData[i].deskNumber),Number(dummyData[i].x),Number(dummyData[i].y)))
+    }
     
   }
-
-  dragEnded(event: CdkDragEnd) {
-    this.state = 'dragEnded';
-    
-  }
-
-  dragMoved(event: CdkDragMove) {
-    var boarder = document.getElementById("boarder");
-    var leftOffset = boarder!.offsetLeft;
-    var toptOffset = boarder!.offsetTop;
-    this.position = `> Position X: ${event.pointerPosition.x-leftOffset-30} - Y: ${event.pointerPosition.y-toptOffset-30}`;
-
-  
-  }
-
 }
+
+
