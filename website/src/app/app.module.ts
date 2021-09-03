859c5884bf55af70ae4da4859d1f1aac706392ec
Matt Harty <matthewnharty@gmail.com>
2021-09-03 12:33:43 +0200

    Added the side nav bar to the home screen along with all the necessary images

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -8,12 +8,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 import { HomepageComponent } from './homepage/homepage.component';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +27,16 @@ import { HomepageComponent } from './homepage/homepage.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule, 
-    DragDropModule, NgbModule,MatCardModule,MatExpansionModule,MatDividerModule,MatListModule,MatButtonModule,MatSidenavModule
+    DragDropModule, 
+    NgbModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatListModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- website/src/app/homepage/homepage.component.ts
+++ website/src/app/homepage/homepage.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
 
-  constructor() { }
+export class HomepageComponent {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
-  ngOnInit(): void {
+  constructor(private observer: BreakpointObserver) {
+
   }
 
+  ngAfterViewInit() {
+    this.observer.observe("").pipe(delay(0.5)).subscribe((result) => {
+        if (result.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+    });
+  }
 }
