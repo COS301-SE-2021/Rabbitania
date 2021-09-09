5271735b4b4197e455b45380a509df1bd9ee4f40
Matt Harty <matthewnharty@gmail.com>
2021-09-09 09:22:54 +0200

    Added correct routing for ai-planner

--- website/src/app/app-routing.module.ts
+++ website/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AIPlannerComponent } from './ai-planner/ai-planner.component';
 import { CovidQuestionnaireComponent } from './covid-questionnaire/covid-questionnaire.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
@@ -8,8 +9,11 @@ const routes: Routes = [
     path: "", component: HomepageComponent
   },
   {
-    path : "covid-questionnaire", component : CovidQuestionnaireComponent
-  }
+    path: "covid-questionnaire", component: CovidQuestionnaireComponent
+  },
+  {
+    path: "ai-planner", component: AIPlannerComponent
+  },
 ];
 
 @NgModule({
