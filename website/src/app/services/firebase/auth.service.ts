f4f99fcf4f53375f1466d69636625687151b819a
Matt Harty <matthewnharty@gmail.com>
2021-09-11 13:02:15 +0200

    Added Auth service to check for valid user and return true.

--- website/src/app/services/firebase/auth.service.ts
+++ website/src/app/services/firebase/auth.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from "firebase/compat/app";
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public authFire: AngularFireAuth) { }
+  private bs = new BehaviorSubject<Boolean>(false); // valid user or not
+
+  constructor(
+    public authFire: AngularFireAuth,
+    private http: HttpClient
+    ) { }
 
   async signIn() {
     const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
@@ -18,7 +25,31 @@ export class AuthService {
     console.log(user.user?.displayName);
     console.log(user.user?.email);
 
-    return user;
+    this.http.post<Boolean>('https://localhost:5001/api/Auth/GoogleLogin', {
+      "displayName": "string",
+      "email": "string",
+      "phoneNumber": "string",
+      "googleImgUrl": "string"
+    }).subscribe(
+        (data) => {
+          if (data) {
+            this.bs.next(true); // valid user
+          } else{
+            this.bs.next(false);
+          }
+        },
+        (error) => {
+          console.log(error);
+          alert('Error with POST request in Autherization Service');
+          return error;
+        }
+    );
+    
+    if(this.bs.value == true){
+      return user;
+    }else{
+      return null;
+    }
   }
 
   async signOut() {
