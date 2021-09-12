4fa38252dc799d1b587f7f5666f4bd335f081dd8
Joseph <u16049943@tuks.co.za>
2021-09-12 12:01:11 +0200

    Finally implemented reactions on noticeboard

---
+++ backend/backend_api/Models/NoticeBoard/Requests/DecreaseEmojiRequest.cs
@@ -0,0 +1,27 @@
+﻿namespace backend_api.Models.NoticeBoard.Requests
+{
+    public class DecreaseEmojiRequest
+    {
+        
+        private string _emojiUTF;
+        private int _noticeboardID;
+        
+        public DecreaseEmojiRequest(string emojiUtf, int noticeboardId)
+        {
+            _emojiUTF = emojiUtf;
+            _noticeboardID = noticeboardId; 
+        }
+
+        public string EmojiUtf
+        {
+            get => _emojiUTF;
+            set => _emojiUTF = value;
+        }
+
+        public int NoticeboardId
+        {
+            get => _noticeboardID;
+            set => _noticeboardID = value;
+        }
+    }
+}
