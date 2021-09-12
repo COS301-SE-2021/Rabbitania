972ff8bb41ec28c3d49ec0f1556fd58efa841618
Deannortje <u17326622@tuks.co.za>
2021-09-12 19:53:30 +0200

    Ai-planner

--- website/src/app/ai-planner/ai-planner.component.ts
+++ website/src/app/ai-planner/ai-planner.component.ts
@@ -7,6 +7,9 @@ import { BreakpointObserver } from '@angular/cdk/layout';
 import { delay } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { NodeServiceService } from '../../app/services/ai-planner/node-service.service';
+import { NodeGetAllRequest } from '../../app/interfaces/ai-planner-node-interface';
+import { Router } from '@angular/router';
+import { faRocket, faUsers, faBriefcase, faThList } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-ai-planner',
@@ -18,7 +21,15 @@ export class AIPlannerComponent implements OnInit {
   //80m x 45m screen -> 40m x 22.5m screen
   //user gets 1m square space to themselves 1m x 1m
   //1600 -> 80 = (1600/10)/2
-  //
+
+  faRocket = faRocket;
+  faUsers = faUsers;
+  faBriefcase = faBriefcase;
+  faThList = faThList;
+
+  sidenav!: MatSidenav;
+  panelOpenState = false;
+
   addNodeForm = this.fb.group({
     email: null,
   });
@@ -28,14 +39,10 @@ export class AIPlannerComponent implements OnInit {
   });
 
 
-  sidenav!: MatSidenav;
 
   constructor(private fb: FormBuilder, private observer: BreakpointObserver,private http: HttpClient,private service: NodeServiceService){
     this.onResize();
   }
-
-  panelOpenState = false;
-  public nodeList:{deskNumber:string, x:string,y:string}[] = dummyData;
 
   ngOnInit(): void {
     //APi call to get nodes pos/name/details
@@ -48,7 +55,6 @@ export class AIPlannerComponent implements OnInit {
     this.editNodeForm = this.fb.group({
         email: null,
       });
-
   }
 
   nodes: MovableNodes[] = [];
@@ -58,13 +64,29 @@ export class AIPlannerComponent implements OnInit {
 
   getDummyNodes(multiplier: number)
   {
+    
     this.nodes = [];
     for(var i =0; i< dummyData.length;i++)
     {
-      //console.log(dummyData[i].deskNumber);
-      this.nodes.push(new MovableNodes(Number(dummyData[i].deskNumber),Number(dummyData[i].x)*multiplier,Number(dummyData[i].y)*multiplier))
+      this.nodes.push(new MovableNodes(Number(dummyData[i].id),Number(dummyData[i].xPos)*multiplier,Number(dummyData[i].yPos)*multiplier,dummyData[i].userEmail,dummyData[i].active))
     }
+  }
 
+  async getNodes(multiplier: number)
+  {
+    
+    var result = await this.service.Get();
+    result.subscribe(data => {
+      if(data){
+        this.nodes = [];
+          for(var i =0; i< data.length;i++)
+          {
+            this.nodes.push(new MovableNodes(Number(data[i].id),Number(data[i].xPos)*multiplier,Number(data[i].yPos)*multiplier,data[i].userEmail,data[i].active))
+          }
+      }
+    });
+
+    
   }
 
 
@@ -78,27 +100,27 @@ export class AIPlannerComponent implements OnInit {
 
     if(this.screenWidth >= 1700)
     {
-      this.getDummyNodes(4);
+      this.getNodes(4);
     }
     else if(this.screenWidth >= 1100)
     {
-      this.getDummyNodes(2.5);
+      this.getNodes(2.5);
     }
     else if(this.screenWidth >= 820)
     {
-      this.getDummyNodes(2);
+      this.getNodes(2);
     }
     else if(this.screenWidth >= 720)
     {
-      this.getDummyNodes(1.6);
+      this.getNodes(1.6);
     }
     else if(this.screenWidth >= 540)
     {
-      this.getDummyNodes(1.0);
+      this.getNodes(1.0);
     }
     else
     {
-      this.getDummyNodes(0.8);
+      this.getNodes(0.8);
     }
   }
 
@@ -133,4 +155,34 @@ export class AIPlannerComponent implements OnInit {
     });
   }
 
+  nodeArray: NodeGetAllRequest[] = [];
+
+  async save()
+  {
+    this.nodeArray = [];
+    this.nodes.forEach(element => {
+      
+      var singleNode:NodeGetAllRequest =
+      {
+        id: element.deskNumber,
+        userEmail: element.userEmail,
+        user: null,
+        xPos: element.newPosx,
+        yPos: element.newPosy,
+        active: element.active,
+      } ;
+
+      this.nodeArray.push(singleNode);
+    });
+      //JSON.stringify(jsonObject);
+      //console.log(JSON.stringify({nodes: this.nodeArray}));
+      var result = await this.service.Save(this.nodeArray);
+        result.subscribe(data => {
+            if(data){
+              console.log(data)
+            }
+          });
+  }
+
+  
 }
