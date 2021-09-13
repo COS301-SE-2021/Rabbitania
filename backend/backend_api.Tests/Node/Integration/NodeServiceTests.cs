e6165b2084240f5151085c16ee8f76a033d1d4f0
jmshltt <u16003251@tuks.co.za>
2021-09-13 12:03:31 +0200

    Node service integration tests

---
+++ backend/backend_api.Tests/Node/Integration/NodeServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using backend_api.Data.Node;
+using backend_api.Data.User;
+using backend_api.Services.Node;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace backend_api.Tests.Node.Integration
+{
+    public class NodeServiceTests
+    {
+        private readonly NodeService _service;
+        private readonly INodeRepository _repository;
+        private NodeContext _context;
+
+        public NodeServiceTests()
+        {
+            var serviceProvider = new ServiceCollection().AddEntityFrameworkNpgsql().BuildServiceProvider();
+            var builder = new DbContextOptionsBuilder<NodeContext>();
+            var env = Environment.GetEnvironmentVariable("CONN_STRING");
+            builder.UseNpgsql(env.ToString())
+                .UseInternalServiceProvider(serviceProvider);
+            
+            _context = new NodeContext(builder.Options);
+            
+
+        }
+    }
+}
