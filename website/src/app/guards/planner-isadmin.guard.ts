b98b433c72473d2089c4a0fc2805de3e5052c210
Matt Harty <matthewnharty@gmail.com>
2021-09-17 15:17:29 +0200

    Done authorization guards for office planner

--- website/src/app/guards/planner-isadmin.guard.ts
+++ website/src/app/guards/planner-isadmin.guard.ts
@@ -1,17 +1,34 @@
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AccessDeniedComponent } from '../access-denied/access-denied.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlannerIsadminGuard implements CanActivate {
+
+  constructor(public model: MatDialog){
+
+  }
+
   canActivate(): boolean  {
     var isAdmin = localStorage.getItem('isAdmin');
     if (isAdmin == "true") {
         return true;
     } else {
+      let modelRef = this.model.open(AccessDeniedComponent,{
+        width: '250px'
+      });
+  
+      modelRef.afterClosed().subscribe(result => {
+        if(result == "close"){
+         return false;
+        }
         return false;
+      });
     }
+    return false;
   } 
 }
