7e5add50749326ca2ea365d1032c5da0c487c8a2
Matt Harty <matthewnharty@gmail.com>
2021-09-19 21:07:30 +0200

    Added req forwarders to the server.js

--- website/server.js
+++ website/server.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("./dist/website"));
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https')
+    res.redirect(`https://www.rabbitania.co.za`)
+  else
+    next()
+});
 
 app.get("/*", function (req, res) {
   res.sendFile("index.html", { root: "dist/website" });
